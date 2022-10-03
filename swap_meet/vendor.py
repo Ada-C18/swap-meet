9d42fcbc8bcb0cab2e3091c36bd5a0f04f955f96
Caitlyn <caitlyn.homet@gmail.com>
2022-10-03 11:58:33 -0400

    created Vendor to complete wave 1, passed all tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
+
+    #adds an item to the inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    #removes an item from the inventory
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+
+
