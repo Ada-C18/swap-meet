1b6d0826fced0673054b9cbec67527af60c4bf3f
Michelle Oh <ohmotrl@gmail.com>
2022-10-03 14:43:55 -0400

    passed all wave1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -3,11 +3,15 @@ class Vendor:
         self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
-        return self.inventory.append(item) 
+        self.inventory.append(item) 
+        return item
     
     def remove(self, item):
         matching_item = item
+        if item not in self.inventory:
+            return False
         self.inventory.remove(matching_item)
-        
+        return matching_item
+
     
 Vendor(inventory = None)        
