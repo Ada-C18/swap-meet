c2d8edc3a8ca1347db247e414de901811f7c3ccf
Sierra <sierra.r.jordan@gmail.com>
2022-10-03 11:15:52 -0700

    created add function in vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,8 @@
 class Vendor:
     def __init__(self, inventory=[]):
         self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        index = len(self.inventory) - 1
+        return self.inventory[index]
