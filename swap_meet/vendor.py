1c1431bc5566d90683cfeef9aeb583e0dcdd1a32
Nina <nina.sohn@gmail.com>
2022-10-03 13:22:51 -0700

    Added category attribute to Item class and added get_by_category method to Vendor class. Wave 2 tests passed
    git status

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from .item import Item
+
 class Vendor:
     
     def __init__(self, inventory = []):
@@ -13,3 +15,10 @@ class Vendor:
             return item
         else:
             return False
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
