18bd43babc05ad5d148cf49af411fc772e3fe774
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-03 12:06:30 -0700

    defined class vendor

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory
+
+    def add(self, new_item):
+        self.inventory(new_item)
+        return new_item
+
+    def remove(self, item_to_remove):
+        if item_to_remove not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item_to_remove)
+        return item_to_remove
