c601908f04a47ccdaabeb06f12f89f998431d839
Yangweiyi Li <lywy.amy@gmail.com>
2022-10-03 13:34:56 -0700

    implement Vendor class and complete unit test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
+from operator import truediv
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory = list()) -> None:
+        self.inventory = inventory
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item) -> bool:
+        try:
+            self.inventory.remove(item)
+            return item
+        except ValueError:
+            return False
