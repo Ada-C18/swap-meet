1e5c56671b074012e1f16d24492f44f363c8382c
Michelle Oh <ohmotrl@gmail.com>
2022-10-03 17:30:47 -0400

    added changes to wave2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,7 @@
 class Vendor:
-    def __init__(self, inventory = None):
+    def __init__(self, inventory = None, item):
         self.inventory = inventory if inventory is not None else []
+        self.item = item
 
     def add(self, item):
         self.inventory.append(item) 
@@ -13,5 +14,8 @@ class Vendor:
         self.inventory.remove(matching_item)
         return matching_item
 
+    def get_by_category(self, category):
+        item_list = self.item(self.category)
+        return item_list
     
 Vendor(inventory = None)        
