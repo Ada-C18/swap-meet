0bdac031da1238a6014771f1f1f10e993963a551
Amanda <amanda.scott17@gmail.com>
2022-10-03 16:59:13 -0700

    edits

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    
+    def __init__(self,inventory):
+        self.inventory = []
+        self.inventory_list = inventory
+    
+    
+def add(self,item):
+        self.inventory_list.append(item)
+        return item
+def remove(self,item_to_remove):
+        if item_to_remove not in self:
+            return False
+        else:
+            self.inventory.remove(item_to_remove)
+            return item_to_remove
+        
