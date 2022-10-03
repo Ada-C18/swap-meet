4ac1fe21a590470804bf8ae756ae7b80ee298c11
Isabella <isabella.vanstedum@gmail.com>
2022-10-03 17:02:31 -0300

    Finished Wave 1, still need to add extra test for test_wave_01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = inventory
+        self.inventory = self.inventory if inventory is not None else []
+    
+    def add(self, item):
+
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory: 
+            item_removed = item
+            self.inventory.remove(item) 
+            return item_removed
+        else:
+            return False
