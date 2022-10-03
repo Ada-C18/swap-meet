f73a8379d5a7f9ccc230a7a91616642782440b82
Maria Fernanda Perez Rojo <mariafperez603@gmail.com>
2022-10-03 14:34:47 -0400

    wave 2 comments

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,19 @@
+# In Wave 2 we will create the `Item` class and the `get_by_category` method.
+
+# - There is a module (file) named `item.py` inside of the `swap_meet` package (folder)
+
+# - Inside this module, there is a class named `Item`
+# - Each `Item` will have an attribute named `category`, which is an empty string by default
+# - When we initialize an instance of `Item`, we can optionally pass in a string with the keyword argument `category`
+# - Instances of `Vendor` have an instance method named `get_by_category`
+#   - It takes one argument: a string, representing a category
+#   - This method returns a list of `Item`s in the inventory with that category
+
 class Item:
-    pass
+    def __init__(self, category=None):
+        self.category = category if category is not None else []
+    
+    def get_by_category(self, category):
+      def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []  
+        
