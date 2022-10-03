431a0ff9fc1c2d3df84f4d17603a2a00642d66ef
Minh <minh.seikel@gmail.com>
2022-10-03 12:51:43 -0700

    Wave1 passed and completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,24 @@
+from swap_meet.item import Item
+
 class Vendor:
+    def __init__(self,inventory = []):
+        self.inventory = inventory
+    
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+    
+    def get_by_category(self,category = ""):
+        list = []
+        if self.category == self.category:
+            list.append(self.item)
+            return list
     
     
     
-    
-    pass
