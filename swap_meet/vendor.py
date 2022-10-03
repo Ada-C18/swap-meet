7e40d1c5e9ed438a3b898fb2c61f7beb7e754261
Thao Le <thaole0325@gmail.com>
2022-10-03 16:20:15 -0400

    wave 1 in progress

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,25 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory):
+        # self.inventory = inventory
+        if len(inventory) >= 1:
+            self.inventory = inventory
+        else:
+            self.inventory = []            
+# default list error here (inventory= None)
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item 
+    
+    def remove(self, item):
+        for item in self.inventory:
+            if item in self.inventory:
+                self.inventory.remove(item)
+                return item
+
+        return False
+        
+    
+
+
