3941e8977d21253fb73645a06a2f270d58bfdb3b
Sierra <sierra.r.jordan@gmail.com>
2022-10-03 12:00:31 -0700

    created swap_items function in Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,3 +22,10 @@ class Vendor:
             if item.category == category:
                 category_items.append(item)
         return category_items
+
+    def swap_items(self, vendor, old_item, new_item):
+        self.inventory.remove(old_item)
+        self.inventory.append(new_item)
+        vendor.inventory.remove(new_item)
+        vendor.inventory.append(old_item)
+        return True
