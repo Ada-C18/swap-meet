07f3bd4db9de39222440525c7e9f855634306298
Tasha <tashachang2@gmail.com>
2022-10-03 12:56:09 -0700

    Completed Wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,12 @@ class Vendor:
             if item.category == category:
                 items.append(item)
         return items
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        self.remove(my_item)
+        self.add(their_item)
+        vendor.add(my_item)
+        vendor.remove(their_item)
+        return True
