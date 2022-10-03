d95f1b6b9d6cabe203c30234dcab1c52457bd483
Lindsey <lindseymarcea@icloud.com>
2022-10-03 16:44:05 -0400

    Passed Wave 1 Tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,21 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory
+
+        if inventory is None:
+            self.inventory = []
+            
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    
+ 
+
