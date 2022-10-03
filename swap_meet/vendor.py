b5670d1a90479fd50adbe234a82e75338671783a
Ariel <gabriellaw15@gmail.com>
2022-10-03 11:07:59 -0700

    Added an innit method and methods add and remove to class Vendor. Passes test 1 in wave 1.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,11 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        self.inventory.remove(item) #is .remove the real thing to use
+        return item
