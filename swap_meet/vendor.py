cf8b8b1aee2d2dc5eedab21bc56c321d28b307ce
Yangweiyi Li <lywy.amy@gmail.com>
2022-10-03 14:58:12 -0700

    implement swap_items method, add a helper function

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,8 @@
-from operator import truediv
+from operator import contains, truediv
 from swap_meet.item import Item
 
 class Vendor:
-    def __init__(self, inventory = list()):
+    def __init__(self, inventory = list()) -> None:
         self.inventory = inventory
     
     def add(self, item):
@@ -22,7 +22,7 @@ class Vendor:
             return item
         except ValueError:
             return False
-    
+
     def get_by_category(self, category):
         """
         Returns a list of Items in the inventory with given category.
@@ -32,3 +32,26 @@ class Vendor:
             if item.category == category:
                 item_list.append(item)
         return item_list
+    
+    def swap_items(self, friend_vendor, my_item, their_item):
+        """
+        Removes the my_item from this Vendor's inventory, and adds it to the friend's inventory.
+        Removes the their_item from the other Vendor's inventory, and adds it to this Vendor's inventory
+        Returns True when above actions are completed.
+        Returns False, if this Vendor's inventory doesn't contain my_item
+        or the friend's inventory doesn't contain their_item. the method returns False
+        """
+        if self.contain(my_item) and friend_vendor.contain(their_item):
+            self.add(friend_vendor.remove(their_item))
+            friend_vendor.add(self.remove(my_item))
+            return True
+        return False
+
+#========== Helper Functions ==========#
+    
+    def contain(self, item) -> bool:
+        """
+        Returns True if the inventory contains the given item, otherwise False.
+        """
+        return item in self.inventory
+    
