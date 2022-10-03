0ac40c5bb54ccb61cd234721fca25ca648dbc232
Ihovanna <90429068+Ihovanna@users.noreply.github.com>
2022-10-03 09:02:35 -0700

    wave 1 (failed 1 last test)

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,27 @@
+from logging import raiseExceptions
+
+
 class Vendor:
-    pass
+#   Initiate the class 'Vendor'
+#   Attribute 'Inventory' = empty list
+#   Intance method #1: add
+#       - return item that was added
+#   Instance method #2: remove
+#       - takes in (1) item
+#       - removes matching item from inventory
+#       - returns the item that was removed-- .pop()
+#       - if no matching item in inventory, return False
+
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+        else:
+            raise exception("False")
+        return item
