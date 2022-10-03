1e20a864c07e939f6f2fbef6d4a26aa5b3d755d1
Shweet Abraha <sabraha0311@gmail.com>
2022-10-03 16:07:15 -0700

    Passing wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.item = item
+        self.inventory.append(self.item)
+        return self.item
+
+    def remove(self, item):
+        self.item = item
+        if self.item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(self.item)
+            return self.item
+            
+
+vender1 = Vendor([])
