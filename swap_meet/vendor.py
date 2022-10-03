def9ec443fe3179c640c0479b79b7d93b38c133d
Anjula Tamang <parkhrinj@gmail.com>
2022-10-03 11:46:47 -0400

    started wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory is not None else []
+        
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        self.inventory.remove(item)
+        if item not in self.inventory:
+            return False
+        return item
+
