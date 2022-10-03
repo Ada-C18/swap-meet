ca499389834e5d31fe3904be64c954d72f650e85
Samantha Hunt <ariaandsam@gmail.com>
2022-10-03 13:33:32 -0700

    Finished wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,23 @@
 #from swap_meet.vendor import Vendor
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category = "", condition = 0):
        self.category = category
-    
+       self.condition = condition
+
     def __str__(self):
         self.str = "Hello World!"
         return self.str
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "This is a no no, unless you want gross stuff."
+        elif self.condition == 1:
+            return "Meh, I still wouldn't go for it, unless it's scrap material."
+        elif self.condition == 2:
+            return "This is approaching okay"
+        elif self.condition == 3:
+            return "This is fine."
+        elif self.condition == 4:
+            return "This is nice, there's just light water damage."
+        else:
+            return "This is perfect."
