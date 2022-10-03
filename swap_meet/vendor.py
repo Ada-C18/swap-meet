4dc3fc80a92e94199fba27a75fb7aef9d58cd6b9
Kat <kmoreno1205@gmail.com>
2022-10-03 17:50:07 -0600

    passes all wave_02 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from .item import Item
+
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
@@ -15,3 +17,12 @@ class Vendor:
         else:
             self.inventory.remove(item)
             return item
+    
+    def get_by_category(self, category):
+        items = [single_item for single_item in self.inventory if category == single_item.category]
+        return items
+        # items = []
+        # for single_item in self.inventory:
+        #     if category == single_item.category:
+        #         items.append(single_item) 
+        # return items
