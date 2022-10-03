80d063193534752dc7882f3d8d067816c3dea7d6
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-03 11:49:13 -0700

    created constructor for Vendor class and two methods addand remove

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,18 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        '''A blueprint for different store inventories.'''
+        # inventory is a list
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = [] 
+    
+    def add(self, item):
+        '''Add an item to the inventory list.'''
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        '''Remove an item from inventory list.'''
+        self.inventory.remove(item)
+        return item 
