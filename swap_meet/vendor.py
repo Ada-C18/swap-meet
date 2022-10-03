097a07d60f94cec6aff24bed1c77208f606138bc
Ariel <gabriellaw15@gmail.com>
2022-10-03 11:10:46 -0700

    Modified class Vendor's constructor mehtod's attribute value. Passed test test_vendor_takes_optional_inventory() in wave 1.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,6 @@
 class Vendor:
     def __init__(self, inventory = []):
-        self.inventory = []
+        self.inventory = inventory
 
     def add(self, item):
         self.inventory.append(item)
