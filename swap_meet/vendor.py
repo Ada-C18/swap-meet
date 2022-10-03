26253ffea42ca9bc0099e33c32717727fb849fb0
Samantha Hunt <ariaandsam@gmail.com>
2022-10-03 14:09:16 -0700

    Finish wave 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -40,3 +40,25 @@ class Vendor:
             self.remove(self.inventory[0])
             return True
         return False
+    
+    def get_best_by_category(self, category):
+        self.current_condition = None
+        self.best_item = None
+        for item in self.inventory:
+            if item.category == category:
+                if self.current_condition == None:
+                    self.current_condition = item.condition
+                    self.best_item = item
+                else:
+                    if self.current_condition < item.condition:
+                        self.current_condition = item.condition
+                        self.best_item = item
+        return self.best_item
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        
+        self.my_item = Vendor.get_best_by_category(self, category = their_priority)
+        other.their_item = Vendor.get_best_by_category(other, category = my_priority)
+        if self.my_item and other.their_item:
+            Vendor.swap_items(self, other, my_item = self.my_item, their_item = other.their_item)
+            return True
+        return False
