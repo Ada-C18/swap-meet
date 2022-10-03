97cb925dbb3aefdc9aca1014fcc0940db8c81573
Maxwell <maxpasqualis@gmail.com>
2022-10-03 13:41:52 -0700

    Added function to remove item from Vendor inventory, completed Wave 01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -7,3 +7,10 @@ class Vendor:
     def add(self, new_item):
         self.inventory.append(new_item)
         return self.inventory[-1]
+
+    def remove(self, item_to_remove):
+        if item_to_remove in self.inventory:
+            removed_item = self.inventory.pop(self.inventory.index(item_to_remove))
+            return removed_item
+        else:
+            return False
