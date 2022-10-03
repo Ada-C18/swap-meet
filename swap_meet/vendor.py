f55e2656cfbd1468c36e68ef9a6f40d36c5f02ea
Dominique <dominiquedaba@gmail.com>
2022-10-03 15:38:32 -0700

    Passed Wave 2 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,12 @@
+from swap_meet.item import Item
+
 class Vendor:
+    #optionally pass in argument "inventory"
     def __init__(self, inventory = None):
-        # if "inventory"
+        # if "inventory" arugment is passed in, use that
         if inventory:
             self.inventory = inventory
+        # otherwise, if there's no inventory argument, initialize and empty list
         else:
             self.inventory = []
     
@@ -15,3 +19,12 @@ class Vendor:
             self.inventory.remove(item)
             return item 
         return False
+
+# do I need to import Item in order for this part of 
+# the function to work?
+    def get_by_category(self, category):
+        inventory_categories = []
+        for item in self.inventory:
+            if item.category == category:
+                inventory_categories.append(item)
+        return inventory_categories
