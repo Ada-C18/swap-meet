fb289692db18102de08b3169284ba85ec55b4d98
viviantomato <vivianyw924@gmail.com>
2022-10-03 14:12:10 -0700

    Wave 1 passed

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,16 @@
 class Item:
-    pass
+    def __init__(self, category = None):
+        if category:
+            self.category = category
+        else:
+            self.category = ""
+
+    def get_by_category(category_str):
+        # a string with the keyword argument category
+        # return list of items in that category
+        item_list =[]
+        next_item = Item(category = category_str)
+        if self.next_item.category_str == category_str:
+            
+
+        
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,21 @@
 class Vendor:
-    pass
+    # def __init__(self, inventory = []):
+    # By having MUTABLE TYPE in DEFAULT argument, HAVE PROBLEMS in INTERGRATION TEST ISSUE
+    # default value none is immutable. mutable xx
+
+    def __init__(self, inventory = None):    
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = []
+
+    def add(self, add_item):
+        self.inventory.append(add_item)
+        return add_item
+
+    def remove(self, remove_item):
+        if remove_item in self.inventory:
+            self.inventory.remove(remove_item)
+            return remove_item
+        else:
+            return False
