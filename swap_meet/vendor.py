9d553d15f31d258161472c04d8d48828afa4d973
Aisha Husseini <ayasha.husseini@gmail.com>
2022-10-03 11:45:28 -0400

    added code to vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,9 @@
 class Vendor:
-    pass
+    def __innit__(self, inventory=None):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.item = item
+        inventory = inventory if inventory is not None else []
+        inventory.append(item)
+        return inventory
