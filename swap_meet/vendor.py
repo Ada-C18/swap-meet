66dc8ad5e5eaab8a9c1e5d41c6645ae14ce22e09
Ariel <gabriellaw15@gmail.com>
2022-10-03 11:22:11 -0700

    Modified class Vendor to return false if item passed in doesn't exist in vendors list. Passed test test_removing_not_found_is_false() in wave 1.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,6 @@
+from xmlrpc.client import FastMarshaller
+
+
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
@@ -7,5 +10,9 @@ class Vendor:
         return item
 
     def remove(self, item):
-        self.inventory.remove(item) #is .remove the real thing to use
+        if item not in self.inventory:
+            return False
+            
+        self.inventory.remove(item) #is .remove the real thing to use?
+        
         return item
