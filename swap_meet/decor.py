a8c73870596d8c474efd2c789b650f6484363863
Ariel <gabriellaw15@gmail.com>
2022-10-03 15:48:00 -0700

    Included class Item as a parent class for classes Clothing, Decor, and Electronics. Removed the condition_description method from those child classes.

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,4 +1,6 @@
-class Decor:
+from swap_meet.item import Item
+
+class Decor(Item):
     def __init__(self, category="Decor", condition=0):
         self.category = category
         self.condition = condition
@@ -6,16 +8,16 @@ class Decor:
     def __str__(self):
         return "Something to decorate your space."
     
-    def condition_description(self):
-        if self.condition == 5:
-            return "Absolutely perfect! I love that for you (^‿◕)"
-        elif self.condition > 4:
-            return "Almost absolutely perfect! Aren't you lucky ☘☘☘"
-        elif self.condition > 3:
-            return "Not too shabby!"
-        elif self.condition > 2:
-            return "They're a little fixer upper"
-        elif self.condition > 1:
-            return "Be gentle with this one, it's a bit (or very) used"
-        else:
-            return "Do an exorcism first...just in case"
+    # def condition_description(self):
+    #     if self.condition == 5:
+    #         return "Absolutely perfect! I love that for you (^‿◕)"
+    #     elif self.condition > 4:
+    #         return "Almost absolutely perfect! Aren't you lucky ☘☘☘"
+    #     elif self.condition > 3:
+    #         return "Not too shabby!"
+    #     elif self.condition > 2:
+    #         return "They're a little fixer upper"
+    #     elif self.condition > 1:
+    #         return "Be gentle with this one, it's a bit (or very) used"
+    #     else:
+    #         return "Do an exorcism first...just in case"
