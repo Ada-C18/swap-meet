f03ced21e0f69d89d1957f2c2e8e40ef4bec7d20
Kat <kmoreno1205@gmail.com>
2022-10-03 16:52:57 -0600

    passes all wave_01 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        if self.inventory == []:
+            self.inventory = [item]
+        else:
+            self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
