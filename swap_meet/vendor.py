0e1d3beaa2e2baa9a7de8ce222083fd16862e9df
Tasha <tashachang2@gmail.com>
2022-10-03 16:15:03 -0700

    Completed Wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -13,17 +13,20 @@ class Vendor:
         return item
 
     def get_by_category(self, category):
-        items = []
-        for item in self.inventory:
-            if item.category == category:
-                items.append(item)
-        return items
+        items = [item for item in self.inventory if item.category == category]
+        return items 
 
     def swap_items(self, vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in vendor.inventory:
             return False
         self.remove(my_item)
         self.add(their_item)
-        vendor.add(my_item)
         vendor.remove(their_item)
+        vendor.add(my_item)
+        return True
+
+    def swap_first_item(self, vendor):
+        if not self.inventory or not vendor.inventory:
+            return False
+        self.inventory[0], vendor.inventory[0] = vendor.inventory[0], self.inventory[0]
         return True
