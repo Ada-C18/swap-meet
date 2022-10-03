a462585dfaf293217ffe0d6ec73d2ad9f7e3426f
Sierra <sierra.r.jordan@gmail.com>
2022-10-03 13:46:35 -0700

    passed swap_best_by_category_reordered

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -88,7 +88,7 @@ def test_swap_best_by_category():
     assert item_f not in jesse.inventory
     assert item_c in jesse.inventory
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_reordered():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -112,14 +112,17 @@ def test_swap_best_by_category_reordered():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
-    # *********************************************************************
-    # ****** Complete Assert Portion of this test **********
-    # *********************************************************************
-    # Assertions should check:
-    # - That result is truthy
-    # - That tai and jesse's inventories are the correct length
-    # - That all the correct items are in tai and jesse's inventories, and that the items that were swapped are not there
+    assert result
+    assert len(tai.inventory) == 3
+    assert item_a in tai.inventory
+    assert item_b in tai.inventory
+    assert item_c not in tai.inventory
+    assert item_f in tai.inventory
+    assert len(jesse.inventory) == 3
+    assert item_d in jesse.inventory
+    assert item_e in jesse.inventory
+    assert item_f not in jesse.inventory
+    assert item_c in jesse.inventory
 
 @pytest.mark.skip
 def test_swap_best_by_category_no_inventory_is_false():
