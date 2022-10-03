9d25d73c2130e17fd5122d7e1b18a9321bb36954
Elsje Slothower <elsje.slothower@gmail.com>
2022-10-03 11:57:38 -0700

    implemented Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,13 @@
 class Vendor:
-    pass
+    def __init__(self, inventory):
+        inventory = []
+        self.inventory_list = inventory
+
+    def add(self, new_item):
+        return self.inventory_list.append(new_item)
+
+    def remove(self, remove_item):
+        if remove_item in self.inventory_list:
+            return self.inventory_list.pop(remove_item)
+        else:
+            return False
