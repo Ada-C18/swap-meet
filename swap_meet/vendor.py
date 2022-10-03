ff83ecadf3dff9da767d626e98d61a6cdaa5fe3c
echo "PATH=/opt/homebrew/bin:$PATH\nexport PATH"  /Users/$(whoami)/.zprofile <PATH="/opt/homebrew/bin:$PATH>
2022-10-03 13:03:43 -0700

    wave one passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,35 @@
+
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+    # inventory is an empty list
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+
+    def add(self, item):
+        self.item = item           # item (this method takes only 1 item)
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item): 
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+# In Wave 1 we will create the `Vendor` class.
+
+# - There is a module (file) named `vendor.py` inside of the `swap_meet` package (folder)
+# - Inside this module, there is a class named `Vendor`
+# - Each `Vendor` will have an attribute named `inventory`, which is an empty list by default
+# - When we instantiate an instance of `Vendor`, we can optionally pass in a list with the keyword argument `inventory`
+
+# - Every instance of `Vendor` has an instance method named `add`, which takes in one item
+# - This method adds the item to the `inventory`
+# - This method returns the item that was added
+
+# - Similarly, every instance of `Vendor` has an instance method named `remove`, which takes in one item
+# - This method removes the matching item from the `inventory`
+# - This method returns the item that was removed
+# - If there is no matching item in the `inventory`, the method should return `False`
