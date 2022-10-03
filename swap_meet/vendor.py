dd27f6dcff19368c067851a1eb39cb70f6cd9401
Ariel <gabriellaw15@gmail.com>
2022-10-03 12:01:09 -0700

    Added get_by_category() method to class Vendor and modified test test_get_no_matching_items_by_category(), which passed. Wave 2.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,4 @@
-from xmlrpc.client import FastMarshaller
-
-
+# import swap_meet.item
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
@@ -12,7 +10,15 @@ class Vendor:
     def remove(self, item):
         if item not in self.inventory:
             return False
-            
+
         self.inventory.remove(item) #is .remove the real thing to use?
         
         return item
+
+    def get_by_category(self, a_category):
+        self.a_category: a_category
+        items = []
+        for item in self.inventory:
+            if item.category == a_category:
+                items.append(item)
+        return items
