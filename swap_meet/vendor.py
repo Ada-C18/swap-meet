3147a754321681b6a22ae663d69ab721573a4994
Aisha Husseini <ayasha.husseini@gmail.com>
2022-10-03 14:31:13 -0400

    waiting for changes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,2 @@
 class Vendor:
-    def __innit__(self, inventory=None):
-        self.inventory = inventory
-
-    def add(self, item):
-        self.item = item
-        inventory = inventory if inventory is not None else []
-        inventory.append(item)
-        return inventory
+    
