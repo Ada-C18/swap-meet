4675213f4797e4ebc23756cd6cf244d74d7f03cb
Puja Mukherjee <pujam836@gmail.com>
2022-10-03 15:48:04 -0700

    wrote code for wave 1 part 1 and ran testcase 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=[]):
+        # we can also do none or false using if else
+        self.inventory = inventory 
+    def add(item):
+        self.inventory.append(item)
+        return item
+    def remove(item):
+        for inventory_item in inventory:
+            if inventory_item==item:
+                self.inventory.remove(item)
+                return item
+        return False
--- tests/unit_tests/test_wave_01.py
+++ tests/unit_tests/test_wave_01.py
@@ -2,54 +2,54 @@
 import pytest
 from swap_meet.vendor import Vendor
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_vendor_has_inventory():
     vendor = Vendor()
     assert len(vendor.inventory) == 0
 
-@pytest.mark.skip
-def test_vendor_takes_optional_inventory():
-    inventory = ["a", "b", "c"]
-    vendor = Vendor(inventory=inventory)
-    assert len(vendor.inventory) == 3
-    assert "a" in vendor.inventory
-    assert "b" in vendor.inventory
-    assert "c" in vendor.inventory
+# @pytest.mark.skip
+# def test_vendor_takes_optional_inventory():
+#     inventory = ["a", "b", "c"]
+#     vendor = Vendor(inventory=inventory)
+#     assert len(vendor.inventory) == 3
+#     assert "a" in vendor.inventory
+#     assert "b" in vendor.inventory
+#     assert "c" in vendor.inventory
 
-@pytest.mark.skip
-def test_adding_to_inventory():
-    vendor = Vendor()
-    item = "new item"
+# # @pytest.mark.skip
+# def test_adding_to_inventory():
+#     vendor = Vendor()
+#     item = "new item"
 
-    result = vendor.add(item)
+#     result = vendor.add(item)
 
-    assert len(vendor.inventory) == 1
-    assert item in vendor.inventory
-    assert result == item
+#     assert len(vendor.inventory) == 1
+#     assert item in vendor.inventory
+#     assert result == item
 
-@pytest.mark.skip
-def test_removing_from_inventory_returns_item():
-    item = "item to remove"
-    vendor = Vendor(
-        inventory=["a", "b", "c", item]
-    )
+# @pytest.mark.skip
+# def test_removing_from_inventory_returns_item():
+#     item = "item to remove"
+#     vendor = Vendor(
+#         inventory=["a", "b", "c", item]
+#     )
 
-    result = vendor.remove(item)
+#     result = vendor.remove(item)
 
-    assert len(vendor.inventory) == 3
-    assert item not in vendor.inventory
-    assert result == item
+#     assert len(vendor.inventory) == 3
+#     assert item not in vendor.inventory
+#     assert result == item
 
-@pytest.mark.skip
-def test_removing_not_found_is_false():
-    item = "item to remove"
-    vendor = Vendor(
-        inventory=["a", "b", "c"]
-    )
+# @pytest.mark.skip
+# def test_removing_not_found_is_false():
+#     item = "item to remove"
+#     vendor = Vendor(
+#         inventory=["a", "b", "c"]
+#     )
 
-    result = vendor.remove(item)
+#     result = vendor.remove(item)
 
-    raise Exception("Complete this test according to comments below.")
-    # *********************************************************************
-    # ****** Complete Assert Portion of this test **********
-    # *********************************************************************
+#     raise Exception("Complete this test according to comments below.")
+#     # *********************************************************************
+#     # ****** Complete Assert Portion of this test **********
+#     # *********************************************************************
