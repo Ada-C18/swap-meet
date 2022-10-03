a81ea1a9e4d85ed534d1aa4ef7fb1cfec793d56d
ashanti <ammanier2@gmail.com>
2022-10-03 16:19:37 -0400

    removed readme instructions from vendor.py and item.py

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,9 @@
 # In Wave 2 we will create the `Item` class and the `get_by_category` method.
 
-# - There is a module (file) named `item.py` inside of the `swap_meet` package (folder)
 
-# - Inside this module, there is a class named `Item`
 # - Each `Item` will have an attribute named `category`, which is an empty string by default
 # - When we initialize an instance of `Item`, we can optionally pass in a string with the keyword argument `category`
-# - Instances of `Vendor` have an instance method named `get_by_category`
-#   - It takes one argument: a string, representing a category
-#   - This method returns a list of `Item`s in the inventory with that category
+
 
 class Item:
     def __init__(self, category=None):
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,27 +1,3 @@
-### Wave 1
-
-# In Wave 1 we will create the `Vendor` class.
-
-# - There is a module (file) named `vendor.py` inside of the `swap_meet` package (folder)
-# - Inside this module, there is a class named `Vendor`
-# - Each `Vendor` will have an attribute named `inventory`, which is an empty list by default
-# - When we instantiate an instance of `Vendor`, we can optionally pass in a list with the keyword argument `inventory`
-
-
-# - Every instance of `Vendor` has an instance method named `add`, which takes in one item
-# - This method adds the item to the `inventory`
-# - This method returns the item that was added
-
-# - Similarly, every instance of `Vendor` has an instance method named `remove`, which takes in one item
-# - This method removes the matching item from the `inventory`
-# - This method returns the item that was removed
-# - If there is no matching item in the `inventory`, the method should return `False`
-
-
-# def add_to_list_ok(word, word_list=None):
-#     word_list = word_list if word_list is not None else []
-#     word_list.append(word)
-#     return word_list
 
 
 
