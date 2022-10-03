18a69b69fc62332c3b8f0adeed1b3bbe6608afd1
Nina <nina.sohn@gmail.com>
2022-10-03 14:15:15 -0700

    Created swap items method in Vendor class. Wave 3 tests passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,4 @@
-from .item import Item
+
 
 class Vendor:
     
@@ -22,3 +22,13 @@ class Vendor:
             if item.category == category:
                 items.append(item)
         return items
+
+    def swap_items(self, other_inventory, my_item, their_item):
+        if their_item in other_inventory.inventory and my_item in self.inventory:
+            other_inventory.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            self.inventory.remove(my_item)
+            other_inventory.inventory.append(my_item)
+        else:
+            return False
+        return self.inventory
