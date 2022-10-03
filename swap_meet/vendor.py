65e4e70a6a40f05d26f1ca58c70213498e207193
Tiffany Tran <tif.tran86@gmail.com>
2022-10-03 14:19:22 -0700

    Wave 1 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,14 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        # inventory = inventory if inventory is not None else []
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
