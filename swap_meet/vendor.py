a5e0351fd9b59bb944884a15a25d98ffb76580de
Elsje Slothower <elsje.slothower@gmail.com>
2022-10-03 12:15:01 -0700

    debugged wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,13 +1,15 @@
 class Vendor:
-    def __init__(self, inventory):
-        inventory = []
-        self.inventory_list = inventory
+    def __init__(self, inventory=None):
+        inventory = inventory if inventory is not None else []
+        self.inventory = inventory
 
     def add(self, new_item):
-        return self.inventory_list.append(new_item)
+        self.inventory.append(new_item)
+        return new_item
 
     def remove(self, remove_item):
-        if remove_item in self.inventory_list:
-            return self.inventory_list.pop(remove_item)
+        if remove_item in self.inventory:
+            self.inventory.remove(remove_item)
+            return remove_item
         else:
             return False
