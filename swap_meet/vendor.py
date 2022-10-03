10733b215bb858e6c09c5e32c90be5d4d9237ef0
Younghee <kwak.younghee@gmail.com>
2022-10-03 14:50:36 -0700

    feat: new class Vendor & test: WAVE 1 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,26 @@
+'''
+module: vendor.py
+    class: Vendor
+        attribute: .inventory
+        mothod: .add .remove
+'''
+
 class Vendor:
-    pass
+
+    def __init__(self, inventory = None):
+        if inventory:
+            self.inventory = inventory
+        else: # default
+            self.inventory = []
+
+    def add(self, one_item):
+    # add the item to the inventory
+        new_inventory = self.inventory.append(one_item)
+        return one_item
+    
+    def remove(self, one_item):
+        if one_item in self.inventory:
+            new_inventory = self.inventory.remove(one_item)
+            return one_item
+        else:
+            return False
