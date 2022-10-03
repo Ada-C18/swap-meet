8342bdf3d82662c2ef6de38e1eec5363bda37d71
Sierra <sierra.r.jordan@gmail.com>
2022-10-03 12:52:27 -0700

    added condition attribute

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,6 +1,7 @@
 class Electronics:
-    def __init__(self, category="Electronics"):
+    def __init__(self, category="Electronics", condition=0.0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
