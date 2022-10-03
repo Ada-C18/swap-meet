cf584fb761a1a22cd9835e1aa830c9517f8994ee
Steffanie <lopezsteffanie8@gmail.com>
2022-10-03 08:59:52 -0700

    working on wave_01

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None,):
+        
+        if inventory == None:
+            self.inventory = []
+        else:
+            self.inventory = inventory
+            
+        
+    def add(self, item):
+        self.inventory.append(item)
+        # return self.item
+    
+    def remove(self, item):
+        if not item:
+            return False
+        
+        self.inventory.remove(item)
+        return self.item
