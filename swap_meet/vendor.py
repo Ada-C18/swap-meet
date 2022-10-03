49ae6e75b3a406efc235234519e43eddc2032529
Melissa <ma.pena94@gmail.com>
2022-10-03 13:14:40 -0700

    Wave 1 completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+
+        self.inventory=inventory
+    
+    def add(self,new_item):
+        self.inventory.append(new_item)
+        return new_item
+
+    def remove(self, removed_item):
+        if removed_item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(removed_item)
+            return removed_item
+        
+
