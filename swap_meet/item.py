e841a928dabdb23ff07ecdfc9390fd87ff855f0c
Marilyn Thai <marilynthai15@gmail.com>
2022-10-03 11:13:04 -0700

    finished test wave 3 :)

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,21 @@
 class Item:
-    pass
+    def __init__(self,category = "",condition = 0):
+        self.category = category
+        self.condition = condition
+    
+    def __str__(self):
+        return 'Hello World!'
+    
+    def condition_description(self):
+        descriptions = {
+            0: "Either no rating or it is really not useable",
+            1: "would not recommend",
+            2: "hopefully it is a good deal",
+            3: "what you would expect",
+            4: "pretty good",
+            5: "NWT"
+            }
+        
+        for rating,description in descriptions.items():
+            if self.condition == rating:
+                return description 
