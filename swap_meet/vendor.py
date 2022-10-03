d70523eae415833372b52aee29bcc9d4768086fa
Michelle Oh <ohmotrl@gmail.com>
2022-10-03 11:57:37 -0400

    added class Vendor to vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,13 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory is not None else []
+
+    def add(self, item):
+        return self.inventory.append(item) 
+    
+    def remove(self, item):
+        matching_item = item
+        self.inventory.remove(matching_item)
+        
+    
+Vendor(inventory = None)        
