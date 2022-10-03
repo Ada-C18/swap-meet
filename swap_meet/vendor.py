8086ca1e5813a27f28d87d59c7cfbec68a489ad9
Emily <sonemily+github@gmail.com>
2022-10-03 18:39:20 -0400

    Create item class, add get_by_category to Vendor.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,3 +12,11 @@ class Vendor:
             
         self.inventory.remove(item)
         return item
+
+    def get_by_category(self, category):
+        item_list = []
+        for item in self.inventory:
+            if item.category == category:
+                item_list.append(item)
+        return item_list
+    # We can do list comprehension for this
