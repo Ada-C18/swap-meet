c2ae63c71b3c3af6137a9d061b0f5756426529fb
Aviva <avivajoy.gars@gmail.com>
2022-10-03 11:15:31 -0600

    passed first test on wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,6 @@
 class Item:
     def __init__(self, category = None):
-        self.category = category if category is not None else []
-    def get_by_category():
-        pass
+        self.category = category if category is not None else ""
+    def get_by_category(self, items):
+        
+        return items
