fba63ae40ab98ca2dd62be88fda84c44bc5e73da
Tazmeen <t.akhtar95@gmail.com>
2022-10-03 14:00:29 -0400

    finished first wave

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,16 @@
 class Vendor:
-    pass
+    def __init__(self,inventory=None):
+        self.inventory = inventory if inventory is not None else [] 
+
+    def add(self,item):
+        self.item = item
+        self.inventory.append(item)
+        return self.item
+    
+    def remove(self, item):
+        self.item = item
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False 
+
