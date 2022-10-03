a9416efef97115f6c2a6690b5fa99a248d2abfd2
Makeesha Pruitt <makeeshalp@gmail.com>
2022-10-03 14:52:47 -0400

    completed wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,11 +1,19 @@
 class Vendor:
 
-    def __innit__(self, inventory = None):
+    def __init__(self, inventory = None):
         self.inventory = inventory
 
         if self.inventory is None:
             self.inventory = []
     def add(self, item):
-        
+        self.item = item
         self.inventory.append(item)
+        return self.item
+
+    def remove(self, item):
+        self.item = item
+        if self.item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
         return self.item
