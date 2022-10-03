6181d925bc872d4f43602ad05848b18f88d34efc
kindra greenawalt <kindra.kathleen@gmail.com>
2022-10-03 17:23:20 -0400

    Wave 2 started, created class Item, and method get_by_category, 1/3 test cases are passing

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+from swap_meet.item import Item
 
 class Vendor:
     def __init__(self, inventory = []):
@@ -15,3 +16,13 @@ class Vendor:
             return item
         else:
             return False 
+    
+    def get_by_category(self,category):
+        # return a list of "items" in the inventory with that category
+        list_of_items = []
+        for item in self.inventory:
+            if category == Item.category:
+                list_of_items.append(item)
+                
+        return list_of_items
+
