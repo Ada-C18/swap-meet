82a7a979c89e2e10f157148aeb3afe30f1bb5292
Milena Souto <yamadasouto@gmail.com>
2022-10-03 11:58:04 -0700

    Write Vendor class with attributes and add and remove methods

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,23 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=[]):
+        # inventory is an empty list by default
+        # we can optionally pass in a list with the keyword argument inventory
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+
+    def remove(self, item):
+        '''
+        Input: item
+        Ouput: item that was removed, or False if no matching item in list
+        Method removes the matching item from the inventory
+        '''
+        # remove() method removes the first occurrence of the element with the specified value
+        # If you need to remove an item by its index number and/or for some reason you want to return (save) the value you removed, use the pop() method instead.
+        if item in self.inventory:
+            self.inventory.pop(item)
+            return item
+        else:
+            # If there is no matching item in the inventory, the method should return False
+            return False
