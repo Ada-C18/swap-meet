dc1519d21d7095058dbe93c9cb5c93fc338ae80c
Isabella <isabella.vanstedum@gmail.com>
2022-10-03 19:23:26 -0300

    test wave 2 executed succesfully

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,9 @@
+from .item import Item
 class Vendor:
     def __init__(self, inventory = None):
         self.inventory = inventory
         self.inventory = self.inventory if inventory is not None else []
-    
+
     def add(self, item):
 
         self.inventory.append(item)
@@ -15,3 +16,11 @@ class Vendor:
             return item_removed
         else:
             return False
+
+    def get_by_category(self, category):
+        item_by_category = []
+        
+        for item in self.inventory:
+            if category == item.category:
+                item_by_category.append(item)
+        return item_by_category
