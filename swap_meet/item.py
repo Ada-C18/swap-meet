de660ed7a8ebca3a27f2be17be8b5a0cab3812cd
Rahel Kemal <rahelskemal@gmail.com>
2022-10-03 16:37:49 -0700

    added instence to items file

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,11 @@
+from swap_meet.vendor import Vendor
+
 class Item:
-    pass
+    def __init__(self, category=None):
+        self.category = category if category is not None else ""
+
+    def get_by_category(self):
+        for item in self.inventory:
+            list_of_cat = []
+            list_of_cat.append(item.category)
+            return list_of_cat
