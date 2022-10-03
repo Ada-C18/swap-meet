51cf0edef1b16b6fd5d38b3731956363586a7302
Yael <yaelechols@gmail.com>
2022-10-03 16:58:09 -0700

    Implements get_by_category() and third wave 2 test case

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,5 @@
+from swap_meet.item import Item
+
 class Vendor:
     
     def __init__(self, inventory=[]):
@@ -12,3 +14,7 @@ class Vendor:
             return False
         self.inventory.remove(item)
         return item
+
+    def get_by_category(self, category):
+        items_by_category = [item for item in self.inventory if item.category == category]
+        return items_by_category
