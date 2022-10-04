fa6a30432fe263aec3acdd45dd93e6df5a30657a
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-04 16:31:17 -0700

    refactored and imported parent class Item

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,11 @@
-class Decor:
+from .item import Item
+
+class Decor(Item):
 
     def __init__(self, condition=0):
-        self.category = "Decor"
-        self.condition = condition
+        super().__init__("Decor", condition)
+        # self.category = "Decor"
+        # self.condition = condition
 
     def __str__(self):
         return "Something to decorate your space."
