b9ea470302c813a989468670e9537f95b9bd539d
Melissa <ma.pena94@gmail.com>
2022-10-04 14:11:40 -0700

    Wave 4 is completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,8 +1,9 @@
 from swap_meet.item import Item
 
 class Vendor:
-    def __init__(self, inventory = []):
-
+    def __init__(self, inventory = None):
+        if inventory is None:
+            inventory=[]
         self.inventory=inventory
     
     def add(self,new_item):
@@ -23,6 +24,35 @@ class Vendor:
                 item_list.append(item)
 
         return item_list
+
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory:
+            if their_item in friend.inventory:
+                self.remove(my_item)
+                friend.add(my_item)
+                friend.remove(their_item)
+                self.add(their_item)
+                
+                return True
+            else:
+                return False 
+        else:
+            return False
+    
+    def swap_first_item(self, friend):
+        if len(self.inventory)== 0 or len(friend.inventory)== 0:
+            return False
+        
+        my_item= self.inventory[0]
+        friend_item= friend.inventory[0]
+
+        self.swap_items(friend,my_item,friend_item)
+        
+        return True
+
+
+
+
 
 
         
