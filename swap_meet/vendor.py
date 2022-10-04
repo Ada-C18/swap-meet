4f04f8975c097005e8f238f0104c65e03bb2b242
Sunny <1sunnymuniz@gmail.com>
2022-10-04 10:07:41 -0600

    modified item class, all tests in wave 2 pass, began wave 3, test 1 passes

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -17,12 +17,33 @@ class Vendor:
         elif item in self.inventory: 
             self.inventory.remove(item)
         return self.item
+
     def get_by_category(self,category=""):
         item_list=[]
         for item in self.inventory:
             if item.category==category:
                 item_list.append(item)
         return item_list
+    
+    def swap_items(self, vendor, my_item, their_item):
+        self.vendor = vendor 
+        self.my_item = my_item # this vendor giving away 
+        self.their_item = their_item # their friend giving away  
+
+        for my_item in self.inventory: 
+            self.inventory.remove(my_item)
+            vendor.inventory.append(my_item) 
+        for their_item in vendor.inventory:
+            vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        else: 
+            return True 
+
+
+    
         
         
 
