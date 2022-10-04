ca4b5b2367090f34438a7c8076b138970b3ffb25
Milena Souto <yamadasouto@gmail.com>
2022-10-03 19:11:34 -0700

    Clean up comments in item module

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,3 @@
-# need to import Vendor class?
-
 class Item:
-    # When we initialize an instance of Item, we can optionally pass in a string with the keyword argument category
     def __init__(self, category=""):
         self.category = category
