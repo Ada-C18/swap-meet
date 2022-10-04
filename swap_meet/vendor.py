9af002ab474f94b75b718a79383f13972a945781
Viktoriia Zolotova <viktoriiazolotova@gmail.com>
2022-10-03 18:47:27 -0700

    Add function get_by_category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,35 +1,40 @@
-from swap_meet.item import Item
+from .item import Item
+#Wave1
+
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
     
     def add(self, item):
+        """
+        Returns the item added to inventory
+        """
         self.inventory.append(item)
         return item
-    
+
     def remove(self, item):
+        """
+        Returns the removed matching item from the inventory 
+        or False if no matching item in the inventory.
+        """
         if item not in self.inventory:
             return False
         self.inventory.remove(item)
         return item
-    
 
+    # Wave 2
+    
     def get_by_category(self, category = ""):
+        """
+        Returns a list of Items in the inventory with that category.
+        """
         items = []
-      
         for item in self.inventory:
             if item.category == category:
                 items.append(item)
         return items
 
-item_a = Item(category="clothing")
-print(f"{item_a.category=}")
-    # def swap_items(self, friend_vendor = [], my_item = Item(), their_items = ""):
-    #     their_items = self.inventory.remove(my_item)
-    #     friend_vendor.append(their_items)
-    #     return True
 
-        
+    
 
-
-
+    
