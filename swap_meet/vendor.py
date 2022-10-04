499f50ffa3bbdc5f00b338cdac86b6f1fa81c927
Paje <ejap99@gmail.com>
2022-10-04 17:46:34 -0500

    wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,13 @@ class Vendor:
             if item.category == category:
                 category_list.append(item)
         return category_list
+    
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            friend.add(my_item) 
+            self.add(their_item)
+            self.remove(my_item)
+            friend.remove(their_item)
+            return True
+        else:
+            return False
