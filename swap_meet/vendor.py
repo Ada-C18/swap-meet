9357a972e88fb938b0e17ac9e24592510d7b3d91
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-04 09:19:52 -0700

    passed all wave 3 tests and integration tests 1,2,3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,3 +22,13 @@ class Vendor:
             if category == item.category:
                 items_by_category.append(item)
         return items_by_category
+
+    def swap_items(self, vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+
+        self.inventory.remove(my_item)
+        vendor.inventory.remove(their_item)
+        self.inventory.append(their_item)
+        vendor.inventory.append(my_item)
+        return True 
