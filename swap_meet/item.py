918ee22c37dcd2aba8f15109afc4471adb3b157a
Lynn <lynn@jansheski.com>
2022-10-04 13:12:56 -0700

    wave 5 complete, passed all unit tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,12 +1,38 @@
 class Item:
     '''add doc string'''
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
     
     def __str__(self):
         if self.category:
             return self.category
         else:
             return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Print 0"
+        elif self.condition == 1:
+            return "Print 1"
+        elif self.condition == 2:
+            return "Print 2"
+        elif self.condition == 3:
+            return "Print 3"
+        elif self.condition == 4:
+            return "Print 4"
+        elif self.condition == 5:
+            return "Print 5"
+        else:
+            return None
 
 
+
+
+
+# '''All three classes and the `Item` class have an instance method named 
+# `condition_description`, which should describe the condition in words based 
+# on the value, assuming they all range from 0 to 5. These can be basic 
+# descriptions (eg. 'mint', 'heavily used') but feel free to have fun with 
+# these (e.g. 'You probably want a glove for this one..."). The one requirement 
+# is that the `condition_description` for all three classes above have the same behavior.'''
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,6 @@
+from xml.dom.expatbuilder import theDOMImplementation
+
+
 class Vendor:
     '''add doc string'''
     def __init__(self, inventory=None):
@@ -18,22 +21,40 @@ class Vendor:
 
     def get_by_category(self, category):
         inventory_by_category = []
+        
         for item in self.inventory:  # items in inventory have categories
             if item.category == category:
                 inventory_by_category.append(item)
+        
         return inventory_by_category      
 
     def swap_items(self, another_vendor, my_item, their_item):
+        
         if len(self.inventory) == 0 or len(another_vendor.inventory) == 0:
             return False
+        
         if my_item in self.inventory and their_item in another_vendor.inventory:
-            print(f"BEFORE {self.inventory=}")
             self.add(their_item)
             self.remove(my_item)
             another_vendor.add(my_item)
             another_vendor.remove(their_item)
-            print(f"AFTER {self.inventory=}")
             return True
+        
         return False
         
+
+### Wave 4
+
+    def swap_first_item(self, another_vendor):
+
+        if len(self.inventory) > 0 and len(another_vendor.inventory) > 0:
+            my_item = self.inventory[0]
+            their_item = another_vendor.inventory[0]
+            return self.swap_items(another_vendor, my_item, their_item)
+
+        else:
+            return False
+
+### Wave 6
+            
 
