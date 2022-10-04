143b3589839f4405eb5a341a4c8129a8d95fd950
Abby <abbychoi97@gmail.com>
2022-10-04 12:37:45 -0700

    refactor class constructor for decor, clothing, and electronics

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,9 +2,9 @@ from swap_meet.item import Item
 
 class Electronics(Item):
     
-    def __init__(self, category="", condition=0):
-        super().__init__(category, condition)
-        self.category = "Electronics"
+    def __init__(self, category="Electronics", condition=0.0, age=0):
+        super().__init__(category, condition, age)
+        # self.category = "Electronics"
     
     def __str__(self):
         return "A gadget full of buttons and secrets."
