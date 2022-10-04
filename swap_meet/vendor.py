f821e57c40301ca5939c9b661bbe86ce69daed87
Carina <checarina@gmail.com>
2022-10-04 14:38:44 -0700

    Implemented Item __str__ method and Vendor swap_items method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,12 @@ class Vendor:
             if item.category == category:
                 category_items.append(item)
         return category_items
+
+    def swap_items(self, friend, my_item, their_item):
+        if my_item not in self.inventory or their_item not in friend.inventory:
+            return False
+        friend.inventory.append(my_item)
+        self.inventory.append(their_item)
+        friend.inventory.remove(their_item)
+        self.inventory.remove(my_item)
+        return True
