20f29eebc8654f4bb5fc94ae0655806d90d063f6
Selam Chaka <selblessed@gmail.com>
2022-10-04 08:50:26 -0400

    Wave 2 all tests passed and some wave 3 tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
 class Item:
-    pass
+    def __init__(self,category= ""):
+        self.category = category
+        
+    def get_category(self):
+        return self.category
+    
+    def __str__(self):
+        return "Hello World!"
+    
+    def my_item(self):
+        pass
+    
+    def their_item(self):
+        pass
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,7 @@
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
-
+        
     def add(self, item):
         self.inventory.append(item)
         return item
@@ -13,3 +13,12 @@ class Vendor:
             self.inventory.remove(item)
             return item
         
+    def get_by_category(self, category):
+        items_list =[]
+        for item in self.inventory:
+            if item.get_category() == category:
+                items_list.append(item)
+        return items_list
+
+    def swap_items(self, friend, my_item, their_item):
+        pass
