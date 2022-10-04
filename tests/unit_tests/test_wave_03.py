abf3b8207d648fafaa153516586716975d2253c7
Puja Mukherjee <pujam836@gmail.com>
2022-10-03 18:44:24 -0700

    ran testcase 4 for wave 3

--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -66,31 +66,31 @@ def test_swap_items_when_my_item_is_missing_returns_false():
     assert not result
 
 # @pytest.mark.skip
-# def test_swap_items_when_their_item_is_missing_returns_false():
-#     item_a = Item(category="clothing")
-#     item_b = Item(category="clothing")
-#     item_c = Item(category="clothing")
-#     fatimah = Vendor(
-#         inventory=[item_a, item_b, item_c]
-#     )
+def test_swap_items_when_their_item_is_missing_returns_false():
+    item_a = Item(category="clothing")
+    item_b = Item(category="clothing")
+    item_c = Item(category="clothing")
+    fatimah = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
 
-#     item_d = Item(category="electronics")
-#     item_e = Item(category="decor")
-#     jolie = Vendor(
-#         inventory=[item_d, item_e]
-#     )
+    item_d = Item(category="electronics")
+    item_e = Item(category="decor")
+    jolie = Vendor(
+        inventory=[item_d, item_e]
+    )
 
-#     result = fatimah.swap_items(jolie, item_b, item_c)
+    result = fatimah.swap_items(jolie, item_b, item_c)
 
-#     assert len(fatimah.inventory) == 3
-#     assert item_d not in fatimah.inventory
-#     assert item_a in fatimah.inventory
-#     assert item_b in fatimah.inventory
-#     assert item_c in fatimah.inventory
-#     assert len(jolie.inventory) == 2
-#     assert item_d in jolie.inventory
-#     assert item_e in jolie.inventory
-#     assert not result
+    assert len(fatimah.inventory) == 3
+    assert item_d not in fatimah.inventory
+    assert item_a in fatimah.inventory
+    assert item_b in fatimah.inventory
+    assert item_c in fatimah.inventory
+    assert len(jolie.inventory) == 2
+    assert item_d in jolie.inventory
+    assert item_e in jolie.inventory
+    assert not result
 
 # @pytest.mark.skip
 # def test_swap_items_from_my_empty_returns_false():
