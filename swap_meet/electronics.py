204cafe0c1e41d465ec51bf447ef3f1516367c22
Margaret <y.margaret.lu+github@gmail.com>
2022-10-04 11:50:00 -0700

    fixed logical error with item's ages

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,6 +1,6 @@
 from swap_meet.item import Item
 class Electronics(Item):
-    def __init__(self,condition=0,age=0):
+    def __init__(self,condition=0,age=20):
         super().__init__(category="Electronics",condition=condition,age=age)
     def __str__(self):
         return "A gadget full of buttons and secrets."
