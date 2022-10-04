d1bc0aaa049299722712031e936686d678893c95
Tazmeen <t.akhtar95@gmail.com>
2022-10-04 17:52:41 -0400

    finished wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,38 +1,45 @@
 class Vendor:
-    def __init__(self, inventory = None):
-        self.inventory = inventory if inventory is not None else [] 
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
 
     def add(self, item):
         self.inventory.append(item)
         return item
-    
+
     def remove(self, item):
         if item in self.inventory:
             self.inventory.remove(item)
             return item
-        return False 
-    
-    def get_by_category(self, category): 
+        return False
+
+    def get_by_category(self, category):
         items_list = []
-        
+
         for item in self.inventory:
             if category == item.category:
                 items_list.append(item)
-        
+
         return items_list
-    
+
     def swap_items(self, vendor, my_item, their_item):
-        
+
         if my_item not in self.inventory or their_item not in vendor.inventory:
             return False
-        
+
         vendor.inventory.append(self.inventory.pop(self.inventory.index(my_item)))
         self.inventory.append(vendor.inventory.pop(vendor.inventory.index(their_item)))
-        
-        # self.inventory.remove(my_item)
-        # other_vendor.inventory.append(my_item)
-        # self.inventory.append(their_item)
-        # other_vendor.inventory.remove(their_item)
-        
+
         return True
 
+    def swap_first_item(self, vendor):
+
+        if not self.inventory or not vendor.inventory:
+            return False
+
+        vendor_item = vendor.inventory.pop(0)
+        self.inventory.append(vendor_item)
+
+        my_item = self.inventory.pop(0)
+        vendor.inventory.append(my_item)
+
+        return True
