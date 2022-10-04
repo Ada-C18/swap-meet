2299cea8d61735d9d359c4826fa9e30d3d6b1513
Minh <minh.seikel@gmail.com>
2022-10-03 21:26:28 -0700

    Clothing, Decor and Electronics classes are created using Item inheritance on Wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,11 @@
 # from swap_meet.vendor import Vendor
 
 class Item:
-    def __init__(self,category = ""):
+    def __init__(self,category = "",condition = 0):
         self.category = category
-    
+        self.condition = condition 
     def __str__(self):
         return ("Hello World!")
+    
+    # def condition_description
     
