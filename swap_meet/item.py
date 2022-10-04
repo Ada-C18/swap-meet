89a6eb8014830e913480a62552d23a3c5b23376d
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-04 15:41:55 -0700

    added condition parameter and attribute to __init__ method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,9 @@
 class Item:
-    def __init__(self, category=""): #, condition=0):
+    def __init__(self, category="", condition=0):
         if category is None:
             self.category = ""
         self.category = category
-        # self.condition = condition
+        self.condition = condition
 
     def __str__(self_):
        return "Hello World!"
