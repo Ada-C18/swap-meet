fd2218278109539bf4bfcbb2ca01edea35b0f8ee
Sika Sarpong <50897076+sikasarp@users.noreply.github.com>
2022-10-03 20:17:59 -0700

    wave 3 dunder method to be completed,all test past except for the str method to overide string

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -19,7 +19,21 @@ class Vendor:
                 category_list.append(item)
         return category_list
 
-    def swap_first_item(self,friend):
+    def swap_items(self, vendor, my_item, their_item):
+        '''If this Vendor's inventory doesn't contain my_item or the friend's inventory doesn't contain their_item,
+    the method returns False'''
+        if my_item not in self.inventory or their_item not in vendor.inventory:
+            return False
+        self.remove(my_item)
+        vendor.add(my_item)
+        vendor.remove(their_item)
+        self.add(their_item)
+        return True
+
+    # def __str__(self):
+        # return f"my_item: {self.my_item}, their_item: {self.their_item}"
+
+    def swap_first_item(self, friend):
         if not self.inventory or not friend.inventory:
             return False
         self.inventory.append(friend.inventory[0])
