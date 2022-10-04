bc715a30d84013f3ed151a99562e9c575733876b
wanjunlan <wanjunlan@gmail.com>
2022-10-03 20:24:23 -0700

    passed wave_1_2_3_4

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,13 @@
 class Item:
-    pass
+
+    def __init__(self, category = None):
+        if category:
+            self.category = category
+        else:
+            self.category = ""
+
+    def __str__(self):
+        return "Hello World!" 
+    
+ 
+            
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,56 @@
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory:
+            self.inventory = inventory
+        else:
+            self.inventory = []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return False
+
+    def get_by_category(self, category):
+        result = []
+        for item in self.inventory:
+            if item.category == category:
+                result.append(item)
+        return result
+
+    def swap_items(self, another_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in another_vendor.inventory:
+            return False
+
+        self.remove(my_item)
+        another_vendor.add(my_item)
+        another_vendor.remove(their_item)
+        self.add(their_item)
+        return True
+
+    def swap_first_item(self, another_vendor):
+        if not self.inventory or not another_vendor.inventory:
+            return False
+
+        self_first = self.inventory.pop(0)
+        another_first = another_vendor.inventory.pop(0)
+        self.inventory.append(another_first)
+        another_vendor.inventory.append(self_first)
+        return True
+
+
+
+
+
+
+
+
+
+
+    
