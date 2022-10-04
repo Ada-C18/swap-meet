81b24ecaa401a060565610bf4366a0f9cb897a3f
Ariel <gabriellaw15@gmail.com>
2022-10-03 19:33:29 -0700

    Deleted some unnecessary attribute defintions and refactored method swap_first_items.

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,12 +12,11 @@ class Vendor:
         if item not in self.inventory:
             return False
 
-        self.inventory.remove(item) #is .remove the real thing to use?
+        self.inventory.remove(item)
         
         return item
 
     def get_by_category(self, a_category):
-        self.a_category: a_category
         items = []
         for item in self.inventory:
             if item.category == a_category:
@@ -25,9 +24,6 @@ class Vendor:
         return items
     
     def swap_items(self, friend, my_item, their_item):
-        self.friend = friend
-        self.my_item = my_item
-        self.their_item = their_item
         if my_item not in self.inventory or their_item not in friend.inventory:
             return False
         else:    
@@ -39,26 +35,14 @@ class Vendor:
             return True
 
     def swap_first_item(self, friend):
-        self.friend = friend
-
         if len(self.inventory) != 0 and len(friend.inventory) != 0:
-            self.add(friend.inventory[0])
-            friend.add(self.inventory[0])
-
-            self.remove(self.inventory[0])
-            friend.remove(friend.inventory[0])
-            
-            return True
+            return self.swap_items(friend, self.inventory[0], friend.inventory[0])
     
     def get_best_by_category(self, category):
 #COULD COME BACK AND TRY USING A MAX FXN AND LAMDA EXPRESSION
 #If there are no items in the inventory that match the category, it returns None
-
-        # instance = self.get_by_category(category)
-        # attribute = instance.a_category
         best_condition = None
         best_condition_val = 0
-
 #Loop through the items of the correct category
         for item in self.get_by_category(category):
 #If the condition of the item is more than the current best condition of all items
@@ -67,16 +51,11 @@ class Vendor:
                 best_condition_val = item.condition
 #Update the value of best_condition with the item with the current highest condition
                 best_condition = item 
-        # if best_condition == "":
-            # return None
+
         return best_condition
 
 
     def swap_best_by_category(self, other, my_priority, their_priority):
-        self.other = other
-        self.my_priority =  my_priority
-        self. their_priority = their_priority
-
         they_want = self.get_best_by_category(their_priority)
         i_want = other.get_best_by_category(my_priority)
         
