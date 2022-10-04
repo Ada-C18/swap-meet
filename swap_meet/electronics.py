e8c4269c3b495cbec73e7cc1624f9fa83a4a449a
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-04 16:31:36 -0700

    refactored and imported parent class Item

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,11 @@
-class Electronics:
+from .item import Item
+
+class Electronics(Item):
 
     def __init__(self, condition=0):
-        self.category = "Electronics"
-        self.condition = condition
+        super().__init__("Electronics", condition)
+        # self.category = "Electronics"
+        # self.condition = condition
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
