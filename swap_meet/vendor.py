50ffdb7765e8500d71343b11931284bbbf3f57d2
hang tran <hangmtran101@gmail.com>
2022-10-03 22:08:17 -0700

    wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,46 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+        
+
+    def add(self, item):
+        self.inventory.append(item)
+        
+        return item
+        
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            
+            return item
+        else:
+             return False
+
+vendor = Vendor()
+inventory = ["a", "b", "c"]
+vendor = Vendor(inventory)
+
+
+
+
+
+
+
+
+
+
+
+
+    # def swap_items(self,vendor_2 my_item, their_item):
+    #     if my_item in self.inventory and their_item in vendor_2.inventory
+
+
+
+
+
+    
+
+ 
+    
+
+    
