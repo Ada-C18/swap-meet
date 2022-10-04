fb8053b37d715131a2c9ddd288f6570b25aafc0a
Aria Stroud <stroudaria@gmail.com>
2022-10-04 11:58:13 -0700

    Adds Aria's wave 1 and 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+        
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+
+    def get_by_category(self, category=""):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
