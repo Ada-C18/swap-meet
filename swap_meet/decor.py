be6bd8180e948906e70e4104cd1a9e6155e62843
Milena Souto <yamadasouto@gmail.com>
2022-10-04 14:45:53 -0700

    Create Decor class with Item parent class and str method

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,11 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+
+class Decor(Item):
+    def __init__(self, category="", condition=0):
+        super().__init__(category, condition)
+        self.category = "Decor"
+
+    def __str__(self):
+        super().__str__()
+        return "Something to decorate your space."
