146e18e3a33f0ce33ab4e1fe9b389270fd4ac69a
Abby <abbychoi97@gmail.com>
2022-10-03 18:51:08 -0700

    established inheritance with item as parent class for decor, electronics, and clothing. passed wave 5 tests

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,11 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    
+    def __init__(self, category="", condition=0):
+        super().__init__(category, condition)
+        self.category = "Electronics"
+    
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
+
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,19 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0: 
+            return "Condition is unknown"
+        elif 1 <= self.condition < 3:
+            return "It needs a lot of love"
+        elif self.condition == 3:
+            return "It just needs a little love"
+        elif self.condition == 4: 
+            return  "Very good"
+        elif self.condition == 5:
+            return "Like New!"
