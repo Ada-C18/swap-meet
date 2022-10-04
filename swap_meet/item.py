449d07dbbe4471520274a62404aaa18bb3ce58bb
Tara <talsaidi@ucdavis.edu>
2022-10-03 18:38:14 -0700

    added child classes of Item: Decor, Electronics, Clothing. added parent method condition_description. passed wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,24 @@
 class Item:
     
-    def __init__(self, category=""):
+    def __init__(self, category="", condition = 0):
         self.category = category
+        self.condition = condition
 
     # def __str__(self) -> str:
         
     def __str__(self):
         return "Hello World!"
 
-    
+    def condition_description(self):
+        if int(self.condition) == 5:
+            return "It's like it's never been used"
+        elif int(self.condition) == 4:
+            return "Great condition"
+        elif int(self.condition) == 3:
+            return "It looks well loved"
+        elif int(self.condition) == 2:
+            return "At least its free"
+        elif int(self.condition) == 1:
+            return "How is this still holding together?"
+        elif int(self.condition) == 0:
+            return "Firestarter"
