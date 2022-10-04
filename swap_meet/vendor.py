bd4d23a5b974a745c58b06a0dd2429ecea7c0336
Alia Athman <aliaathmann@gmail.com>
2022-10-04 15:51:38 -0400

    Adds the class Item and the instance method get_by_category to vendor, passing all tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+from swap_meet.item import Item
 class Vendor:
     def __init__(self,inventory=None):
         self.inventory = inventory if inventory is not None else []
@@ -9,5 +10,10 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
-    
+    def get_by_category(self, category):
+        items_in_category = []
+        for item in self.inventory:
+            if item.category == category:
+                items_in_category.append(item)
+        return items_in_category 
     
