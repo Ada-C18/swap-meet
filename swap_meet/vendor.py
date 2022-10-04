c76e6ef00e7baaa946b2938c0264a3562b870508
Atefeh Mirzaei <atifaayman@gmail.com>
2022-10-03 17:47:58 -0700

    finishing up to wave3 and passing the all tests,forgot to commit after passing wave1 and wave2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,36 @@
-class Vendor:
-    pass
+from .item import Item
+
+class Vendor():
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+
+    def get_by_category(self, category):
+        items_list = []
+        for item in self.inventory:
+            if item.category == category:
+                items_list.append(item)
+
+        return items_list
+    
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory :
+            friend.add(my_item)
+            self.remove(my_item)
+            self.add(their_item)
+            friend.remove(their_item)
+            return True
+        return False
+        
+
+
+
