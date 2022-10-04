d87ef1b33c4f69298debd5de35158de0e7fc597f
masinakh <nakhjirimasi@gmail.com>
2022-10-04 09:55:14 -0700

    forgottt ocommit , Finish wave 1,2,3,4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,49 @@
+from swap_meet.item import Item
 class Vendor:
-    pass
+
+    def __init__(self,inventory = []):
+        
+        self.inventory = inventory
+     
+
+    def add(self,item):
+        self.inventory.append(item)  
+        return item
+
+    def remove(self,item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item 
+
+    def get_by_category(self,category):
+        list_items =[]
+
+        for item in self.inventory:
+            if item.category == category:
+                list_items.append(item)
+
+        return list_items   
+
+
+    def swap_items(self,friend,my_item,their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            friend.add(my_item)
+            self.remove(my_item)
+            self.add(their_item)
+            friend.remove(their_item)
+            return True
+        return False 
+
+    def swap_first_item(self, friend):
+        if not self.inventory or not friend.inventory :
+            return False
+        self.swap_items(friend, self.inventory[0],friend.inventory[0])
+        return True       
+
+
+
+
+
+
+        
