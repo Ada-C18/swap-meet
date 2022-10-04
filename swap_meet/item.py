efc054130a3f9287fefd909660fd80e3f1e2136b
Milena Souto <yamadasouto@gmail.com>
2022-10-04 14:57:52 -0700

    Add logic to condition_description method in Item parent class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,6 +9,20 @@ class Item:
         '''
         return "Hello World!"
 
-    def condition_description(self, condition):
-        # if condition ==
-        pass
+    def condition_description(self):
+        # handle condition if it's a float
+        if type(self.condition) == float:
+            self.condition = int(self.condition)
+
+        if self.condition == 0:
+            return "Bad"
+        elif self.condition == 1:
+            return "Almost bad"
+        elif self.condition == 2:
+            return "Meh"
+        elif self.condition == 3:
+            return "Good"
+        elif self.condition == 4:
+            return "Great"
+        elif self.condition == 5:
+            return "Nothing is perfect, but it almost is"
