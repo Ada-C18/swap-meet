13920b5dad09a41374102dd74f97562de7d52197
Ariel <gabriellaw15@gmail.com>
2022-10-03 19:34:59 -0700

    Cleaned up some comments.

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -7,17 +7,3 @@ class Electronics(Item):
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
-    
-    # def condition_description(self):
-    #     if self.condition == 5:
-    #         return "Absolutely perfect! I love that for you (^‿◕)"
-    #     elif self.condition > 4:
-    #         return "Almost absolutely perfect! Aren't you lucky ☘☘☘"
-    #     elif self.condition > 3:
-    #         return "Not too shabby!"
-    #     elif self.condition > 2:
-    #         return "They're a little fixer upper"
-    #     elif self.condition > 1:
-    #         return "Be gentle with this one, it's a bit (or very) used"
-    #     else:
-    #         return "Do an exorcism first...just in case"
