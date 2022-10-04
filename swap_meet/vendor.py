e9ee066149cc5779b9d2b5ed524badf6d9c266ad
Siyi Ji <monkeyqueenstacy@gmail.com>
2022-10-04 10:36:57 -0400

    wave2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,9 @@
+from operator import itemgetter
+from typing import ItemsView
+
+from swap_meet.item import Item
+
+
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
@@ -12,4 +18,11 @@ class Vendor:
         else:
             self.inventory.remove(item)
             return item
-        
+    
+    def get_by_category(self, category):
+        filtered_list = []
+        for item in self.inventory:
+            if item.get_category() == category:
+                filtered_list.append(item)
+        return filtered_list
+
