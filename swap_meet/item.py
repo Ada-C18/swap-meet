c06e3a5c43184f178507c946bfd1ec49819d279a
kimiahasani <kn.improve@gmail.com>
2022-10-03 19:00:42 -0700

    modified item and vendor

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
-class Item:
-    pass
+
+class Item():
+    def __init__(self, category = ""):
+        self.category = category
+
+    def __str__(self) -> str:
+        return "Hello World!"
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,49 @@
+# import item to access Item class in Vendor class
+from swap_meet.item import Item
+
+#creat Vendor class has __init__ , add , remove , get_by_category, wap_items, exciting_inventory function
 class Vendor:
-    pass
+    def __init__(self, inventory= []):
+        self.inventory = inventory
+    
+    def add(self,item):
+        # this function add item to inventory list
+        self.inventory.append(item)
+        return item
+    def remove(self, item):
+        # this function remove item into inventory
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self,category):
+        category_list = []
+        for item in self.inventory:
+            if item.category == category:
+                category_list.append(item)
+        return category_list
+
+    def swap_items(self, friend, my_item, their_item):
+        if not self.exciting_inventory(my_item):
+            return False
+        elif not friend.exciting_inventory(their_item):
+            return False
+        else:
+            self.remove(my_item)
+            friend.add(my_item)
+            friend.remove(their_item)
+            self.add(their_item)
+            return True
+            
+    def exciting_inventory(self, item):
+        return item in self.inventory
+        # if item in self.inventory:
+        #     return True
+        # else:
+        #     return False
+
+        
+            
+            
