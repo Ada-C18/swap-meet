9167c0398f85f6e178f0f1ffb576cacb6638146b
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-04 10:01:20 -0700

    updated method swap_items in vendor.py

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -29,4 +29,16 @@ class Vendor:
                 category_list.append(i)
         return category_list
             
-
+    def swap_items(self, other_vendor, my_item, their_item):
+        '''Remove an item from this Vendor's inventory and swaps
+        with another vendor.'''
+        # remove and append
+        # is there a more efficient way?
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.add(their_item)
+            self.remove(my_item)
+            other_vendor.add(my_item)
+            other_vendor.remove(their_item)
+            return True
+        else:
+            return False 
