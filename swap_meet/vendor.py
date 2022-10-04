c34a396818a630b34b8e7e1551a00065f98d711e
Neida <neida@duck.com>
2022-10-04 11:34:45 -0400

    passes wave 3 and integration

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,3 +15,13 @@ class Vendor:
 
     def get_by_category(self, category):
         return [item for item in self.inventory if item.category == category]
+    
+    def swap_items(self, vendor_friend, my_item, their_item):
+        if my_item not in self.inventory or their_item not in vendor_friend.inventory:
+            return False
+        my_result = self.remove(my_item)
+        their_result = vendor_friend.remove(their_item)
+        self.add(their_item)
+        vendor_friend.add(my_item)
+        return True
+        
