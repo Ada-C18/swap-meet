651516975f43c8c45173ff1e45b39b4347536536
bukunmig <bgesinde44@icloud.com>
2022-10-04 14:12:45 -0700

    update wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,8 +25,24 @@ class Vendor:
         if my_item not in my_inventory or their_item not in their_inventory:
             return False
 
-        my_inventory.remove(my_item)
         my_inventory.append(their_item)
-        their_inventory.remove(their_item)
         their_inventory.append(my_item)
+        my_inventory.remove(my_item)
+        their_inventory.remove(their_item)
         return True
+
+
+    def swap_first_item(self,vendor):
+        my_inventory = self.inventory 
+        their_inventory = vendor.inventory
+
+        if len(my_inventory)==0 or len(their_inventory)==0:
+            return False
+        
+        my_item = my_inventory[0]
+        their_item = their_inventory[0]
+        return self.swap_items(vendor, my_item, their_item)
+
+    
+
+        
