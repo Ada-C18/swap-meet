e110ec54c4d9c3423ffd1e99c64e5ca92159a680
Kat <kmoreno1205@gmail.com>
2022-10-03 18:36:06 -0600

    refactored swap_items

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,19 +21,15 @@ class Vendor:
         return items
 
     def swap_items(self, other_vendor, self_item, other_vendor_item):
-        if self_item in self.inventory and other_vendor_item in other_vendor.inventory:
-            #Adds vendor2's swap item and removes vendor's swap item from vendor's inventory
-            for single_item in self.inventory:
-                if single_item == self_item:
-                    self.inventory.append(other_vendor_item)
-                    self.inventory.remove(self_item)
-            #Adds vendor's swap item and removes vendor2's swap item from vendor2's inventory
-            for single_item in other_vendor.inventory:
-                if single_item == other_vendor_item:
-                    other_vendor.inventory.append(self_item)
-                    other_vendor.inventory.remove(other_vendor_item)
-            return True
         #Returns false if vendor's swap item is not in vendor's inventory 
         #or if vendor2's swap item is not in vendor2's inventory
-        else:
+        if self_item not in self.inventory or other_vendor_item not in other_vendor.inventory:
             return False
+        #Adds vendor2's swap item and removes vendor's swap item from vendor's inventory
+        self.inventory.append(other_vendor_item)
+        self.inventory.remove(self_item)
+        #Adds vendor's swap item and removes vendor2's swap item from vendor2's inventory
+        other_vendor.inventory.append(self_item)
+        other_vendor.inventory.remove(other_vendor_item)
+        return True
+
