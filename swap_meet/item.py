80bb6ffe4f19133a24c2b2a6dc4f8a58c868f0f0
Milena Souto <yamadasouto@gmail.com>
2022-10-04 14:42:24 -0700

    Modify Item class and add 2 more attributes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,14 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         '''
         Method to stringify/convert an instance of Item to a str
         '''
         return "Hello World!"
+
+    def condition_description(self, condition):
+        # if condition ==
+        pass
