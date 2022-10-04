8ffb25902ab83b2624e186220c95cefd4f0da2a4
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-04 16:30:17 -0700

    created condition_description method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,5 +5,26 @@ class Item:
         self.category = category
         self.condition = condition
 
-    def __str__(self_):
+    def __str__(self):
        return "Hello World!"
+
+    def condition_description(self):
+        # is there a more concise way? for loop?
+        if self.condition == 0:
+            zero_condition_description = "Yo, this is bad. Reconsider."
+            return zero_condition_description
+        elif self.condition == 1:
+            one_condition_description = "You could probably find better... but this'll do."
+            return one_condition_description
+        elif self.condition == 2:
+            two_condition_description = "If you're okay with some dings and things."
+            return two_condition_description
+        elif self.condition == 3:
+            three_condition_description = "Lots of life left in this one."
+            return three_condition_description
+        elif self.condition == 4:
+            four_condition_description = "Oooh, quite the find! Barely a scratch."
+            return four_condition_description
+        elif self.condition == 5:
+            five_condition_description = "ULTRA RARE, PERFECT CONDITION."
+            return five_condition_description
