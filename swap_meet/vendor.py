8bfc1380dd2f16d39f599646627f0dfefc0fb610
Steffanie <lopezsteffanie8@gmail.com>
2022-10-04 11:17:24 -0700

    passed wave_03, wave_04, wave_05

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -27,3 +27,38 @@ class Vendor:
             if category == item.category:
                 new_list.append(item)
         return new_list
+    
+# ***** added for wave_03 *****
+    def swap_items(self, Vendor, my_item, their_item):
+        
+        if my_item in self.inventory and their_item in Vendor.inventory:
+            # removes item from self inventory and adds it to friend's inventory
+            self.inventory.remove(my_item)
+            Vendor.inventory.append(my_item)
+            
+            # removes item from friend's inventory and adds it to self inventory
+            Vendor.inventory.remove(their_item)
+            self.inventory.append(their_item)
+            
+            return True
+        
+        else:
+            return False
+        
+# ***** added for wave_04 *****
+    def swap_first_item(self, Vendor):
+        if not self.inventory or not Vendor.inventory:
+            return False
+        # grabs first items from inventory's
+        self_first_item = self.inventory[0]
+        vendor_first_item = Vendor.inventory[0]
+
+        #removes first items from inventory's
+        del self.inventory[0]
+        del Vendor.inventory[0]
+
+        # swaps first items and adds to beginning of inventory
+        self.inventory.insert(0, vendor_first_item)
+        Vendor.inventory.insert(0, self_first_item)
+
+        return True
