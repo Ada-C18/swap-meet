ef26a5d208030406a819e4a74e67b7a30482af47
galinaramaniuk <galina.sergarz@gmail.com>
2022-10-04 16:05:07 -0400

    Created Clothing, Decor and Electronics classes. Tests are passed. Need refactoring

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,24 @@
+import math
+
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
   
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "Terrible"
+        elif self.condition <= 1:
+            return "Meh"
+        elif self.condition <= 2:
+            return "Average"
+        elif self.condition <= 3:
+            return "Above Average"
+        elif self.condition <= 4:
+            return "Pretty Good"
+        elif self.condition <= 5:
+            return "Excellent"
