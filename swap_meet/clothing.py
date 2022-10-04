d3194eed6a2493dd46a796dadb58c331afa88980
Milena Souto <yamadasouto@gmail.com>
2022-10-04 14:41:20 -0700

    Create Clothing class with Item parent class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,13 @@
-class Clothing:
-    pass
+from .item import Item
+# from swap_meet.item import Item
+
+
+# class Clothing:
+class Clothing(Item):
+    def __init__(self, category="", condition=0):
+        super().__init__(category, condition)
+        self.category = "Clothing"
+
+    def __str__(self):
+        super().__str__()
+        return "The finest clothing you could wear."
