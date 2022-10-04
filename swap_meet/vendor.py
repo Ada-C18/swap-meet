3c179af8d991ba7daf1fbf547e7c644744a01261
francescafr <hello@francesca.fyi>
2022-10-04 12:09:38 -0700

    built vendor class, passed wave 1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,19 @@
 class Vendor:
-    pass
+     # inventory (list of items)
+    # accept item class instances
+
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if not item in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item) #explore other removal methods
+            return item
+    
+       
