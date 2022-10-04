098efbcafb3c66b5cf0ff10c0fe37188ffc6e16f
Puja Mukherjee <pujam836@gmail.com>
2022-10-03 17:31:59 -0700

    ran testcase 1 for wave 3 and wrote code to return Hello World

--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -2,7 +2,7 @@ import pytest
 from swap_meet.vendor import Vendor
 from swap_meet.item import Item
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_item_overrides_to_string():
     item = Item()
 
@@ -10,125 +10,125 @@ def test_item_overrides_to_string():
 
     assert stringified_item == "Hello World!"
 
-@pytest.mark.skip
-def test_swap_items_returns_true():
-    item_a = Item(category="clothing")
-    item_b = Item(category="clothing")
-    item_c = Item(category="clothing")
-    fatimah = Vendor(
-        inventory=[item_a, item_b, item_c]
-    )
+# @pytest.mark.skip
+# def test_swap_items_returns_true():
+#     item_a = Item(category="clothing")
+#     item_b = Item(category="clothing")
+#     item_c = Item(category="clothing")
+#     fatimah = Vendor(
+#         inventory=[item_a, item_b, item_c]
+#     )
 
-    item_d = Item(category="electronics")
-    item_e = Item(category="decor")
-    jolie = Vendor(
-        inventory=[item_d, item_e]
-    )
+#     item_d = Item(category="electronics")
+#     item_e = Item(category="decor")
+#     jolie = Vendor(
+#         inventory=[item_d, item_e]
+#     )
 
-    result = fatimah.swap_items(jolie, item_b, item_d)
+#     result = fatimah.swap_items(jolie, item_b, item_d)
 
-    assert len(fatimah.inventory) == 3
-    assert item_b not in fatimah.inventory
-    assert item_a in fatimah.inventory
-    assert item_c in fatimah.inventory
-    assert item_d in fatimah.inventory
-    assert len(jolie.inventory) == 2
-    assert item_d not in jolie.inventory
-    assert item_e in jolie.inventory
-    assert item_b in jolie.inventory
-    assert result
+#     assert len(fatimah.inventory) == 3
+#     assert item_b not in fatimah.inventory
+#     assert item_a in fatimah.inventory
+#     assert item_c in fatimah.inventory
+#     assert item_d in fatimah.inventory
+#     assert len(jolie.inventory) == 2
+#     assert item_d not in jolie.inventory
+#     assert item_e in jolie.inventory
+#     assert item_b in jolie.inventory
+#     assert result
 
-@pytest.mark.skip
-def test_swap_items_when_my_item_is_missing_returns_false():
-    item_a = Item(category="clothing")
-    item_b = Item(category="clothing")
-    item_c = Item(category="clothing")
-    fatimah = Vendor(
-        inventory=[item_a, item_b, item_c]
-    )
+# @pytest.mark.skip
+# def test_swap_items_when_my_item_is_missing_returns_false():
+#     item_a = Item(category="clothing")
+#     item_b = Item(category="clothing")
+#     item_c = Item(category="clothing")
+#     fatimah = Vendor(
+#         inventory=[item_a, item_b, item_c]
+#     )
 
-    item_d = Item(category="electronics")
-    item_e = Item(category="decor")
-    jolie = Vendor(
-        inventory=[item_d, item_e]
-    )
+#     item_d = Item(category="electronics")
+#     item_e = Item(category="decor")
+#     jolie = Vendor(
+#         inventory=[item_d, item_e]
+#     )
 
-    result = fatimah.swap_items(jolie, item_e, item_d)
+#     result = fatimah.swap_items(jolie, item_e, item_d)
 
-    assert len(fatimah.inventory) == 3
-    assert item_d not in fatimah.inventory
-    assert item_a in fatimah.inventory
-    assert item_b in fatimah.inventory
-    assert item_c in fatimah.inventory
-    assert len(jolie.inventory) == 2
-    assert item_d in jolie.inventory
-    assert item_e in jolie.inventory
-    assert not result
+#     assert len(fatimah.inventory) == 3
+#     assert item_d not in fatimah.inventory
+#     assert item_a in fatimah.inventory
+#     assert item_b in fatimah.inventory
+#     assert item_c in fatimah.inventory
+#     assert len(jolie.inventory) == 2
+#     assert item_d in jolie.inventory
+#     assert item_e in jolie.inventory
+#     assert not result
 
-@pytest.mark.skip
-def test_swap_items_when_their_item_is_missing_returns_false():
-    item_a = Item(category="clothing")
-    item_b = Item(category="clothing")
-    item_c = Item(category="clothing")
-    fatimah = Vendor(
-        inventory=[item_a, item_b, item_c]
-    )
+# @pytest.mark.skip
+# def test_swap_items_when_their_item_is_missing_returns_false():
+#     item_a = Item(category="clothing")
+#     item_b = Item(category="clothing")
+#     item_c = Item(category="clothing")
+#     fatimah = Vendor(
+#         inventory=[item_a, item_b, item_c]
+#     )
 
-    item_d = Item(category="electronics")
-    item_e = Item(category="decor")
-    jolie = Vendor(
-        inventory=[item_d, item_e]
-    )
+#     item_d = Item(category="electronics")
+#     item_e = Item(category="decor")
+#     jolie = Vendor(
+#         inventory=[item_d, item_e]
+#     )
 
-    result = fatimah.swap_items(jolie, item_b, item_c)
+#     result = fatimah.swap_items(jolie, item_b, item_c)
 
-    assert len(fatimah.inventory) == 3
-    assert item_d not in fatimah.inventory
-    assert item_a in fatimah.inventory
-    assert item_b in fatimah.inventory
-    assert item_c in fatimah.inventory
-    assert len(jolie.inventory) == 2
-    assert item_d in jolie.inventory
-    assert item_e in jolie.inventory
-    assert not result
+#     assert len(fatimah.inventory) == 3
+#     assert item_d not in fatimah.inventory
+#     assert item_a in fatimah.inventory
+#     assert item_b in fatimah.inventory
+#     assert item_c in fatimah.inventory
+#     assert len(jolie.inventory) == 2
+#     assert item_d in jolie.inventory
+#     assert item_e in jolie.inventory
+#     assert not result
 
-@pytest.mark.skip
-def test_swap_items_from_my_empty_returns_false():
-    fatimah = Vendor(
-        inventory=[]
-    )
+# @pytest.mark.skip
+# def test_swap_items_from_my_empty_returns_false():
+#     fatimah = Vendor(
+#         inventory=[]
+#     )
 
-    item_d = Item(category="electronics")
-    item_e = Item(category="decor")
-    jolie = Vendor(
-        inventory=[item_d, item_e]
-    )
+#     item_d = Item(category="electronics")
+#     item_e = Item(category="decor")
+#     jolie = Vendor(
+#         inventory=[item_d, item_e]
+#     )
 
-    nobodys_item = Item(category="clothing")
+#     nobodys_item = Item(category="clothing")
 
-    result = fatimah.swap_items(jolie, nobodys_item, item_d)
+#     result = fatimah.swap_items(jolie, nobodys_item, item_d)
 
-    assert len(fatimah.inventory) == 0
-    assert len(jolie.inventory) == 2
-    assert not result
+#     assert len(fatimah.inventory) == 0
+#     assert len(jolie.inventory) == 2
+#     assert not result
 
-@pytest.mark.skip
-def test_swap_items_from_their_empty_returns_false():
-    item_a = Item(category="clothing")
-    item_b = Item(category="clothing")
-    item_c = Item(category="clothing")
-    fatimah = Vendor(
-        inventory=[item_a, item_b, item_c]
-    )
+# @pytest.mark.skip
+# def test_swap_items_from_their_empty_returns_false():
+#     item_a = Item(category="clothing")
+#     item_b = Item(category="clothing")
+#     item_c = Item(category="clothing")
+#     fatimah = Vendor(
+#         inventory=[item_a, item_b, item_c]
+#     )
 
-    jolie = Vendor(
-        inventory=[]
-    )
+#     jolie = Vendor(
+#         inventory=[]
+#     )
 
-    nobodys_item = Item(category="clothing")
+#     nobodys_item = Item(category="clothing")
 
-    result = fatimah.swap_items(jolie, item_b, nobodys_item)
+#     result = fatimah.swap_items(jolie, item_b, nobodys_item)
 
-    assert len(fatimah.inventory) == 3
-    assert len(jolie.inventory) == 0
-    assert not result
+#     assert len(fatimah.inventory) == 3
+#     assert len(jolie.inventory) == 0
+#     assert not result
