aa185e394d50e1d9a92584e642ede2a7f0913d97
Nina <nina.sohn@gmail.com>
2022-10-04 16:00:47 -0700

    Created get_best_by_category and swap_best_by_category methods in the Vendor class. Completed Wave 6 tests, all passed. Passed Wave 4,5,6 integration test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -47,5 +47,28 @@ class Vendor:
             return False
         return self.inventory
 
+    def get_best_by_category(self, category):
+        best_condition = 0
+        best_item = ""
+        category_items = self.get_by_category(category)
+        if not category_items:
+            best_item = None
+        else:
+            for item in category_items:
+                if item.condition > best_condition:
+                    best_condition = item.condition
+                    best_item = item
+        return best_item
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        my_swap = self.get_best_by_category(their_priority)
+        their_swap = other.get_best_by_category(my_priority)
+        if not self.inventory or not other.inventory:
+            return False
+        elif my_swap not in self.inventory or their_swap not in other.inventory:
+            return False
+        else:
+            self.swap_items(other, my_swap, their_swap)
+        return self.inventory
 
 
