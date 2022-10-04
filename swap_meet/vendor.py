7275728a4b053cf416e42fe26bbb89258f1e9e60
Samiya <samiyai96@gmail.com>
2022-10-03 22:58:45 -0400

    WE DID TEST WAVE 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,6 @@
+from swap_meet.item import Item
+
+
 class Vendor:
     def __init__(self,inventory=[]):
         self.inventory = inventory
@@ -14,5 +17,15 @@ class Vendor:
         elif item in self.inventory: 
             self.inventory.remove(item)
         return self.item
+    def get_by_category(self,category=""):
+        item_list=[]
+        for item in self.inventory:
+            if item.category==category:
+                item_list.append(item)
+        return item_list
+        
+        
+
+
 
 
