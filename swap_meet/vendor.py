e74e1c09b590f1d67008c79d408b158309e7ac8d
Julia Park <pje106@gmail.com>
2022-10-04 13:24:42 -0700

    wave1, 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,39 @@
+from .item import Item
+
+
+
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        if inventory is None:
+            inventory = []
+        self.inventory = inventory
+        
+
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+
+        self.inventory.remove(item)
+        return item
+    
+    def get_by_category(self,category = None):
+        if category is None:
+            category = ""
+        category_items = []
+        for item in self.inventory:
+            if item.category == category:
+                category_items.append(item)
+
+        return category_items
+
+    
+
+    def swap_items(self, my_item, their_item):
+        if 
+
+    
