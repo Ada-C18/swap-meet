2005ee82185f6dd3115240744a3add9640ef09a9
Alaere Nekekpemi <alaeren00@gmail.com>
2022-10-04 13:54:12 -0700

    minor changes

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,9 @@
 class Item:
-    def __init__(self, category = ''):
+    def __init__(self, category = '', condition = 0):
         self.category = category
 
     def __str__(self):
         return f"Hello World!{self.category}"
+
+    def condition_description(self):
+        return f"There are {self.condition} {self.category}"
