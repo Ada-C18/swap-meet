62698712926431da3b4ee48d4076efc3a82939ca
Liat <liathirsh@gmail.com>
2022-10-03 21:04:12 -0400

    wave 1 complete minus the test

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,25 +1,22 @@
+from re import L
+
+
 class Vendor:
     
-    def __init__(self, inventory):
-        # self.inventory = inventory
-        if len(inventory) >= 1:
-            self.inventory = inventory
-        else:
-            self.inventory = []            
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    
 # default list error here (inventory= None)
 
     def add(self, item):
         self.inventory.append(item)
         return item 
-    
+        
     def remove(self, item):
-        for item in self.inventory:
-            if item in self.inventory:
-                self.inventory.remove(item)
-                return item
-
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
         return False
         
-    
-
 
