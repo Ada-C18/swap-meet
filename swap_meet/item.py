62527543671b41360b79830f99041431bce54dca
Yun <ly.yun811@gmail.com>
2022-10-03 21:40:42 -0700

    worked on Wave 1 & Wave 2.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,8 @@
+from swap_meet.vendor import Vendor
+
 class Item:
-    pass
+    def __init__(self, category):
+        self.category = category
+
+    def get_by_category(category_name):
+        return 
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
+from pickle import FALSE
+from swap_meet.item import Item
+
 class Vendor:
-    pass
+    def __init__(self):
+        self.inventory = []
+
+    def add_item(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove_item(self, item):
+        if item in self.inventory:
+            return item
+        return False
