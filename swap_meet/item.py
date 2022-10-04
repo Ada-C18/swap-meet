535f3167e36ccd5a0f8b12efe80cb73f518e7d66
Dominique <dominiquedaba@gmail.com>
2022-10-04 13:56:30 -0700

    Passed Wave 5 tests

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,11 @@
-# Wave 2
+# Wave 2, 5
 #=========================================
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
+        if condition:
+            self.condition = condition
+        else:
+            self.condition = 0
         if category:
             self.category = category
         else:
@@ -12,4 +16,19 @@ class Item:
     # write stringify method that returns "Hello World!"
     def __str__(self):
         return "Hello World!"
-        
+
+# Wave 5
+#=========================================  
+    def condition_description(self):
+        if self.condition == 0:
+            return "Wouldn't wear this if offered money"
+        elif self.condition == 1:
+            return "Heavily used"
+        elif self.condition == 2:
+            return "Moderately used"
+        elif self.condition == 3:
+            return "Lightly used"
+        elif self.condition == 4: 
+            return "Like new"
+        elif self.condition == 5:
+            return "New"
