6cc4b6b42c3222f8922de9702e907dbe0eda0ee0
Maxwell <maxpasqualis@gmail.com>
2022-10-04 14:04:07 -0700

    Added condition descriptions for Item objects, completed Wave 05

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,3 +5,17 @@ class Item:
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition >= 5:
+            return "This item is brand new!"
+        elif self.condition >= 4:
+            return "This item is slightly worn, but still in good shape."
+        elif self.condition >= 3:
+            return "This item is clearly secondhand, but not too bad."
+        elif self.condition >= 2:
+            return "This item needs cleaning, but might still be OK."
+        elif self.condition >= 1:
+            return "This item is in pretty bad shape."
+        else:
+            return "This item is so old and worn, it's almost unrecognizable."
