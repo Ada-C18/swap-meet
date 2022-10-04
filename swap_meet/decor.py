0df2a9c5559f0217863c010e9e40036817f3ed13
June Valentino <vintydesign@gmail.com>
2022-10-04 14:36:01 -0700

    passed wave 5 tests

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,23 @@
+import math
+
 class Decor:
-    pass
+    def __init__(self, condition = 0.0):
+        self.category = "Decor"
+        self.condition = condition
+
+    def __str__(self):
+        return "Something to decorate your space."
+
+    def condition_description(self):
+        if math.floor(self.condition) == 0:
+            return "damaged"
+        elif math.floor(self.condition) == 1:
+            return "poor"
+        elif math.floor(self.condition) == 2:
+            return "average"
+        elif math.floor(self.condition) == 3:
+            return "good"
+        elif math.floor(self.condition) == 4:
+            return "mint"
+        elif math.floor(self.condition) == 5:
+            return "new"
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,23 @@
+import math
+
 class Electronics:
-    pass
+    def __init__(self, condition = 0.0):
+        self.category = "Electronics"
+        self.condition = condition
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
+
+    def condition_description(self):
+        if math.floor(self.condition) == 0:
+            return "damaged"
+        elif math.floor(self.condition) == 1:
+            return "poor"
+        elif math.floor(self.condition) == 2:
+            return "average"
+        elif math.floor(self.condition) == 3:
+            return "good"
+        elif math.floor(self.condition) == 4:
+            return "mint"
+        elif math.floor(self.condition) == 5:
+            return "new"
