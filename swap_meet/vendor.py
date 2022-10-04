104b63ea76b97845f9f7bc7caf550b97ce3b2c5f
Nancy Lee <nramirez289@gmail.com>
2022-10-04 13:33:39 -0700

    completed first wave passing all tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,14 @@
 class Vendor:
-    def __init__(self, inventory):
+    def __init__(self, inventory=[]):
         self.inventory = inventory
 
-    def adding_an_item(self):
-        vendor_inventory = self.inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        if item not in self.inventory:
+            return False
+        else:
+            self.inventory.remove(item)
+            return item
