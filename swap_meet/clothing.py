a8bb8bb90f4ce21963ffc1e6212d6f914c463343
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-04 15:44:02 -0700

    added condition attribute to __init__ method

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,7 +1,8 @@
 class Clothing:
 
-    def __init__(self):
+    def __init__(self, condition=0):
         self.category = "Clothing"
+        self.condition = condition
     
     def __str__(self_):
        return "The finest clothing you could wear."
