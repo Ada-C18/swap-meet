6f89cee107220267fd7f53e21ebba196a651757d
Irene <choireneh@gmail.com>
2022-10-03 22:56:43 -0400

    Creates item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,7 @@
 class Item:
-    pass
+
+    def __init__(self, category = ""):
+        """
+        sets up the Item instance with an optional category attribute
+        """
+        self.category = category
