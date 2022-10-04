efaecb5b1a81f50cacda76cecb6f7f118d89dfcf
francescafr <hello@francesca.fyi>
2022-10-04 15:46:08 -0700

    passed wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,9 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, condition = 0.0):
+        super().__init__()
+        self.category = "Electronics"
+        self.condition = condition
+    def __str__(self):
+        return f"A gadget full of buttons and secrets."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,26 @@
 class Item:
-    def __init__(self, category = ""):
-        self.category = category
-    
+    def __init__(self, category=None, condition=None):
+        if not category:
+            self.category = ""
+        else:
+            self.category = category
+
+        if not condition:
+                self.condition = 0.0
+        else:
+            self.condition = condition
+
     def __str__(self):
         return("Hello World!")
+
+    def condition_description(self):
+        if self.condition <= 1:
+            return f"Terrible condition"
+        elif self.condition <= 2:
+            return f"Bad condition"
+        elif self.condition <=3:
+            return f"Meh condition"
+        elif self.condition <=4:
+            return f"Decent condition"
+        else:
+            return f"Great condition"
