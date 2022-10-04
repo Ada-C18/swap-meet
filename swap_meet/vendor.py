1c48ad0a55cbf5566c6c3e189b9582bd7c113087
Tiffany Tran <tif.tran86@gmail.com>
2022-10-03 22:15:08 -0700

    Wave 3 passed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -16,9 +16,18 @@ class Vendor:
     
     def get_by_category(self, category):
         list_by_category = []
-        
+
         # I don't know what else to call it... so it's thing for now =/
         for thing in self.inventory:
             if thing.category == category:
                 list_by_category.append(thing)
         return list_by_category
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.remove(my_item)
+        other_vendor.add(my_item)
+        self.add(their_item)
+        other_vendor.remove(their_item)
+        return True
