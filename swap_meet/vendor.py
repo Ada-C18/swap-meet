f12891a37d774881b89775c364778fe85bde2bc2
Annie <annie.gallagher7@gmail.com>
2022-10-04 15:25:45 -0600

    cleaned up lines

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -3,7 +3,7 @@ from .item import Item
 class Vendor:
     def __init__(self, inventory = None):
         self.inventory = inventory if inventory is not None else []
-    
+
     def add(self, item):
         self.inventory.append(item)
         return item
@@ -13,8 +13,8 @@ class Vendor:
             return False
         self.inventory.remove(item)
         return item
-        
-    def get_by_category(self,category):
+
+    def get_by_category(self, category):
         category_items = [item for item in self.inventory if item.category == category]
         return category_items
     
@@ -31,18 +31,13 @@ class Vendor:
         their_inventory.remove(their_item)
         return True
 
-
-    def swap_first_item(self,vendor):
+    def swap_first_item(self, vendor):
         my_inventory = self.inventory 
         their_inventory = vendor.inventory
 
-        if len(my_inventory)==0 or len(their_inventory)==0:
+        if len(my_inventory) == 0 or len(their_inventory) == 0:
             return False
-        
+
         my_item = my_inventory[0]
         their_item = their_inventory[0]
         return self.swap_items(vendor, my_item, their_item)
-
-    
-
-        
