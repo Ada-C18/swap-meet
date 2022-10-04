75faefd6515e9936f54d76e532078849e6a60ac1
bukunmig <bgesinde44@icloud.com>
2022-10-04 13:54:36 -0700

    updated wave

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -18,3 +18,15 @@ class Vendor:
         category_items = [item for item in self.inventory if item.category == category]
         return category_items
     
+    def swap_items(self, vendor, my_item, their_item):
+        my_inventory = self.inventory 
+        their_inventory = vendor.inventory
+
+        if my_item not in my_inventory or their_item not in their_inventory:
+            return False
+
+        my_inventory.remove(my_item)
+        my_inventory.append(their_item)
+        their_inventory.remove(their_item)
+        their_inventory.append(my_item)
+        return True
