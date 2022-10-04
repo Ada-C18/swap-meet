4b15609d5a4db3e08f720131d999ccae2048e923
Irene <choireneh@gmail.com>
2022-10-04 16:26:26 -0400

    Added stringify method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,3 +5,6 @@ class Item:
         sets up the Item instance with an optional category attribute
         """
         self.category = category
+
+    def __str__(self):
+        return "Hello World!"
