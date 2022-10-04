7df8369b45002cd9ef7c838af5331e730d98b86a
Atefeh Mirzaei <atifaayman@gmail.com>
2022-10-04 09:56:50 -0700

    Refactoring the method swap_first_item in wave4 and refactoring the constractor of the clothing , Decor and electronics classes at wave5

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -31,14 +31,20 @@ class Vendor():
             return True
         return False
 
+    # def swap_first_item(self, friend):
+    #     if len(self.inventory) != 0 and len(friend.inventory) != 0:
+    #         friend.add(self.inventory[0])
+    #         self.remove(self.inventory[0])
+    #         self.add(friend.inventory[0])
+    #         friend.remove(friend.inventory[0])
+    #         return True
+    #     return False
+
     def swap_first_item(self, friend):
-        if len(self.inventory) != 0 and len(friend.inventory) != 0:
-            friend.add(self.inventory[0])
-            self.remove(self.inventory[0])
-            self.add(friend.inventory[0])
-            friend.remove(friend.inventory[0])
-            return True
-        return False
+        if not self.inventory or not friend.inventory :
+            return False
+        self.swap_items(friend, self.inventory[0],friend.inventory[0])
+        return True
 
         
 
