d52a26900ff4214fbaa636acbfa7de67a58c7b06
Yun <ly.yun811@gmail.com>
2022-10-04 10:11:01 -0700

    continue wave 1-3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,12 @@
 from swap_meet.vendor import Vendor
 
 class Item:
-    def __init__(self, category):
-        self.category = category
+    def __init__(self):
+        self.category = ""
 
-    def get_by_category(category_name):
-        return 
+    def get_by_category(self, category_name):
+        list_item = []
+        for item in list(Item):
+            if category_name == self.category:
+                list_item.append(item)
+                return list_item
