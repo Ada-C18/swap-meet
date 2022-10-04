c32870d792a8cd399e1bd160a52ba3f43d9e66ad
Rahel Kemal <rahelskemal@gmail.com>
2022-10-03 20:06:18 -0700

    get by category instance

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,11 +1,6 @@
 # # =========================WAVE 2==============================================
 
-class Item:
+
+class Item():
     def __init__(self, category=None):
         self.category = category if category is not None else ""
-
-    def get_by_category(self):
-        for item in self.inventory:
-            list_of_cat = []
-            list_of_cat.append(item.category)
-            return list_of_cat
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,15 +1,18 @@
 
 # =========================WAVE 1==============================================
+# from unicodedata import category
+from swap_meet.item import Item
 
 # create a vendor class
 
-class Vendor:
+
+class Vendor():
 
 # each vendor will have INVENTORY attribute
 # instantiate an instance of VENDOR we can optionally pass in a list for INVENTORY--> inventory=[]
 
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
 
 # every instance of vendor has ADD instance method
 # ADDs one item to inventory
@@ -32,3 +35,17 @@ class Vendor:
         return item
 
 # IF no matching items in INVENTORY, return False
+
+#loop through each item in the inventory 
+#take out the category associated with that item 
+#return a list with the items in that category 
+
+    def get_by_category(self, category=""):
+        list_of_items = []
+        for item in self.inventory:
+            if item.category == category:
+                list_of_items.append(item)
+        return list_of_items
+
+
+
