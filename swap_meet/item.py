4251016da9312293361e788baa56fad8899460d8
Milena Souto <yamadasouto@gmail.com>
2022-10-04 13:30:57 -0700

    Write method to stringify an instance of Item

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,9 @@
 class Item:
     def __init__(self, category=""):
         self.category = category
+
+    def __str__(self):
+        '''
+        Method to stringify/convert an instance of Item to a str
+        '''
+        return "Hello World!"
