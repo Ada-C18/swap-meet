e9a374c5f2fdcebe82bae67fc3f7cf4bd8acb5b7
Erika Torres Figueroa <torrespurple@gmail.com>
2022-10-03 21:36:05 -0400

    added stringify method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -2,13 +2,5 @@ class Item:
     def __init__(self, category=None):
         category = category if category is not None else ""
         self.category = category
-    
-    # if str(self):
-    #     return "Hello World!"
-    
-    # def stringified_item(self, item):
-    #     try: 
-    #         self.item = item
-    #         return str(item)
-    #     except AssertionError:
-    #         return "Hello World!"
+    def __str__(self):
+        return f"Hello World!"
