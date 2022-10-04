5071eb717d66afe0ee36c57f96c1b3c0a542ec7a
Silvia <sx0771@gmail.com>
2022-10-04 14:37:18 -0400

    wave1 completed

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,20 @@
-class Vendor:
-    pass
+from swap_meet.item import Item
+
+class Vendor: 
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory is not None else []
+    
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else: 
+            return False
+
+    def get_by_category(self, category):
+        self.category = Item(category)
+        return self.inventory.category
