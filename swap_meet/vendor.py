5668f4bb2587200d530ccde29810a32d67e43265
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-10-04 12:00:56 -0700

    Finished Wave 2 and made an assert for third test in test_wave_02

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,8 @@
+from swap_meet.item import Item
+
+item1 = Item()
+item1.get_by_category()
+
 class Vendor:
     def __init__(self, inventory = []):
         self.inventory = inventory
@@ -12,3 +17,11 @@ class Vendor:
         else:
             self.inventory.remove(item)
             return item
+
+    def get_by_category(self, category):
+        item_list = []
+        for item in self.inventory:
+            if item.category == category:
+                item_list.append(item)
+            
+        return item_list
