1c16fe4956abb3dabb9fc886584f18903326f128
Valentina Ruiz <v.zruiz06@gmail.com>
2022-10-05 06:17:11 -0700

    started wave 5

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,7 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    
+    def __str__(self):
+        return "The finest clothing you could wear."
+
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -7,3 +7,5 @@ class Item:
     def __str__(self):
         return "Hello World!"
     
+    def condition_description(self):
+        pass
