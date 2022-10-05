22724e6f71d4de5db39c8dfca3d2ca243a8e5685
ivystrayed <ivystrayed@gmail.com>
2022-10-05 12:28:58 -0700

    Vendor Class creates a list of items and has function to add and remove items

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    # init attributes: inventory(list)
+    def __init__(self, inventory =  []):
+        self.inventory = inventory
+
+
+
+    # method to add items to inventory
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+        
+
+
+    # method to remove items from inventory
+    def remove(self, item):
+        try:
+            self.inventory.remove(item)
+        except(ValueError):
+            return False
+
+        return item
