3dcab646b4b2b188d7796570bc6fa089ea4b4989
Alia Athman <aliaathmann@gmail.com>
2022-10-04 21:57:40 -0400

    Refactors swap_first_meet instance method of Vendor class for readability

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,10 +21,10 @@ class Vendor:
     def swap_first_item(self, friend):
         if len(self.inventory) == 0 or len(friend.inventory) == 0:
             return False
-        else:
-            self.inventory.append(friend.inventory[0])
-            friend.inventory.append(self.inventory[0])
-            self.inventory.remove(self.inventory[0])
-            friend.inventory.remove(friend.inventory[0])
+        
+        self.inventory.append(friend.inventory[0])
+        friend.inventory.append(self.inventory[0])
+        self.inventory.remove(self.inventory[0])
+        friend.inventory.remove(friend.inventory[0])
         return True
         
