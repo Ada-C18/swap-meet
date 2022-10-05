f2c15edc4f4c71d82993f7405dcdfcf0a0a39e87
dainiz <dainiz.almazan@gmail.com>
2022-10-05 13:18:22 -0700

    Adds swap_first_item() method to Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -29,3 +29,15 @@ class Vendor:
         friend.inventory.append(my_removed_item)
         self.inventory.append(friend_removed_item)
         return True
+
+    def swap_first_item(self, friend):
+        if not self.inventory or not friend.inventory:
+            return False
+        
+        my_item = self.inventory.pop(0)
+        friend_item = friend.inventory.pop(0)
+
+        friend.inventory.append(my_item)
+        self.inventory.append(friend_item)
+
+        return True
