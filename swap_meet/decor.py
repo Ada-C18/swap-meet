e4a428c319c9a9066b57582c926540cbb4c0ac64
Sika Sarpong <50897076+sikasarp@users.noreply.github.com>
2022-10-05 07:47:56 -0700

    wave_5 almost completed

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,7 @@
 class Decor:
-    pass
+    def __init__(self, category="Decor", condition=0):
+        self.category = category
+        self.condition = condition
+
+    def __str__(self):
+        return "Something to decorate your space."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,12 @@
 class Item:
-    def __init__(self, category=""):
+    '''
+    All three classes and the Item class have an attribute called condition,
+    which can be optionally provided in the initializer. The default value 
+    should be 0.
+    '''
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
