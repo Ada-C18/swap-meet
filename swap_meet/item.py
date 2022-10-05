a3d998892a857fe3973375dd0721b5ce52671033
wanjunlan <wanjunlan@gmail.com>
2022-10-04 20:28:52 -0700

    passed test wave_5 and integration_1_2_3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,13 +1,28 @@
 class Item:
 
-    def __init__(self, category = None):
-        if category:
-            self.category = category
-        else:
-            self.category = ""
+    def __init__(self, category = "", condition = 0):
+        self.category = category
+        self.condition = condition 
+        
 
     def __str__(self):
         return "Hello World!" 
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "mint"
+        elif self.condition == 1:
+            return "brand new"
+        elif self.condition == 2:
+            return "almost new"
+        elif self.condition == 3:
+            return "used"
+        elif self.condition == 4:
+            return "heavily used"
+        elif self.condition == 5:
+            return "You probably want a glove for this one..."
+            
+
     
  
             
