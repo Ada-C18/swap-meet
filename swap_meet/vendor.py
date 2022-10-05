84e1511a972e4b0385c97f97b50a01f9c77e23db
dainiz <dainiz.almazan@gmail.com>
2022-10-04 19:34:52 -0700

    Adds swap_items() method to Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,3 +15,17 @@ class Vendor:
     
     def get_by_category(self,category):
         return [item for item in self.inventory if item.category == category]
+    
+    def swap_items(self, friend, my_item, their_item):
+        if my_item not in self.inventory or their_item not in friend.inventory:
+            return False
+
+        my_index = self.inventory.index(my_item)
+        friend_index = friend.inventory.index(their_item)
+
+        my_removed_item = self.inventory.pop(my_index)
+        friend_removed_item = friend.inventory.pop(friend_index)
+
+        friend.inventory.append(my_removed_item)
+        self.inventory.append(friend_removed_item)
+        return True
