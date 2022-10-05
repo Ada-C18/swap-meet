09ae624c2805c9ad10301a8ad8596d9296526301
nina-tuyen tran <ninatuyentran@gmail.com>
2022-10-04 20:53:48 -0700

    edits to the electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,8 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+class Electronics(Item):
+    def __init__(self, category = "", condition = 0.0):
+        self.category = "Electronics"
+        self.condition = 0.0
+    def __str__(self):
+        self.Electronics = "A gadget full of buttons and secrets."
+        return self.Electronics
