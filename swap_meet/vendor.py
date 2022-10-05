06cf06798588d6c2e9e9c0d4548a9e97a6fb3af2
Aria Stroud <stroudaria@gmail.com>
2022-10-05 14:46:24 -0700

    Adds functions for wave 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -39,3 +39,43 @@ class Vendor:
         friend.inventory.remove(friend.inventory[0])
         return True
         
+    def get_best_by_category(self, category):
+        items_by_category = self.get_by_category(category)
+        items_by_condition = []
+        for item in items_by_category:
+            items_by_condition.append(item.condition)
+        sorted_condition = sorted(items_by_condition)
+        for item in items_by_category:
+            if item.condition == sorted_condition[-1] and item.category == category:
+                return item
+        return None 
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        items_by_category = self.get_by_category(their_priority)
+        items_by_condition = []
+        for item in items_by_category:
+            items_by_condition.append(item.condition)
+        sorted_condition = sorted(items_by_condition)
+
+        other_items_category = other.get_by_category(my_priority)
+        other_items_by_condition = []
+
+        for item in other_items_category:
+            other_items_by_condition.append(item.condition)
+        sorted_other_condition = sorted(other_items_by_condition)
+        for item in self.inventory:
+            if not item.category in self.inventory == their_priority:
+                return False
+        for item in other.inventory:
+            if not item.category in other.inventory == my_priority:
+                return False
+    
+            self.inventory.append(sorted_other_condition[-1])
+            other.inventory.append(sorted_condition[-1])
+            self.inventory.remove(sorted_condition[-1])
+            other.inventory.remove(sorted_other_condition[-1])
+
+            
+        
+        
+
