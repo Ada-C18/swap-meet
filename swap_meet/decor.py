a2e5e08af54abdc57df76108b7d6c0461f4626a3
Tiffany Tran <tif.tran86@gmail.com>
2022-10-04 18:01:08 -0700

    Wave 5 passed

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,9 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+class Decor(Item):
+    def __init__(self, category=None, condition=0):
+        self.category = "Decor"
+        self.condition = float(condition)
+
+    def __str__(self):
+        return "Something to decorate your space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,9 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, category=None, condition=0):
+        self.category = "Electronics"
+        self.condition = float(condition)
+
+    def __str__(self):
+        return "A gadget full of buttons and secrets."
