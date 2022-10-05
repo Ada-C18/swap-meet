2c24a89d54938a46d5d1d80f130ce462cd60f874
Michelle Oh <ohmotrl@gmail.com>
2022-10-05 14:18:38 -0400

    expecting merge conflict

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,8 +3,8 @@ import numbers
 
 class Item:
     def __init__(self, condition = 0, category = ""):
-        self.category = category
-        self.condition = condition
+        self.category = category if category is not None else ""
+        self.condition = condition if condition is not None else 0
     
     def __str__(self):
         return 'Hello World!'
@@ -13,7 +13,6 @@ class Item:
         condition_list = ["heavily used", "moderately used", "slightly used", "used", "almost new", "brand new"]
         
         for i in range(len(condition_list)):
-            print(i)
             if i == self.condition:
                 return condition_list[i]
 
@@ -24,3 +23,5 @@ class Item:
 # matched condition with index
 
 #self.condtion get us a numbers
+
+# pytest tests/unit_tests/test_wave_05.py    
