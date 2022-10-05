1bcdd635927a6f89cdcc485384389110658a64a7
Yael <yaelechols@gmail.com>
2022-10-04 17:54:23 -0700

    Fixes typo

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,7 +3,7 @@ from swap_meet.item import Item
 class Electronics(Item):
     
     def __init__(self, condition=0):
-        super().__init__(condition)
+        self.category = "Electronics"
         self.condition = condition
 
     def __str__(self):
