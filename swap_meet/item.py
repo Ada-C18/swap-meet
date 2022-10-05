c40257830463babd75423bc33342e5b5458cffda
Alyssa Reyes <reyesalyssa18@gmail.com>
2022-10-05 14:29:15 -0700

    refactored condition_description method to dictionary from if-elif

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,22 +9,17 @@ class Item:
        return "Hello World!"
 
     def condition_description(self):
-        # is there a more concise way? for loop?
-        if self.condition == 0:
-            zero_condition_description = "Yo, this is bad. Reconsider."
-            return zero_condition_description
-        elif self.condition == 1:
-            one_condition_description = "You could probably find better... but this'll do."
-            return one_condition_description
-        elif self.condition == 2:
-            two_condition_description = "If you're okay with some dings and things."
-            return two_condition_description
-        elif self.condition == 3:
-            three_condition_description = "Lots of life left in this one."
-            return three_condition_description
-        elif self.condition == 4:
-            four_condition_description = "Oooh, quite the find! Barely a scratch."
-            return four_condition_description
-        elif self.condition == 5:
-            five_condition_description = "ULTRA RARE, PERFECT CONDITION."
-            return five_condition_description
+        # changed from if-elif to dictionary
+        condition_dict = {
+            0:"Yo this is trash.",
+            1:"Definitely used. Perhaps too much.",
+            2:"If you're okay with some dings and things.",
+            3:"Lots of life left if you handle with care.",
+            4:"Barely even a scractch.",
+            5:"ULTRA RARE PIECE, NWOT CONDITION.",
+
+        }
+        if self.condition in condition_dict:
+            return condition_dict[self.condition]
+            
+       
