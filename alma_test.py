8dc866bc5f320e217f4f40726e9157c2fc3a4b80
Alma <almafernandez@yahoo.com>
2022-10-04 22:14:35 -0700

    ignore this file

---
+++ alma_test.py
@@ -0,0 +1,15 @@
+# class Vendor:
+#     def __init__(self, inventory=[]):
+#         self.inventory = inventory
+        
+        
+# vendor = Vendor()
+
+# print(len(vendor.inventory))
+
+
+class Item:
+    def __init__(self, category=''):
+        self.category = category
+        
+    
