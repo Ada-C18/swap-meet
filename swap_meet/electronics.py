bd0043758c942970b5a0f221d1c123f59cc1c13a
Yael <yaelechols@gmail.com>
2022-10-04 17:53:30 -0700

    Fixes error in condition attribute across Item related classes

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -4,7 +4,7 @@ class Electronics(Item):
     
     def __init__(self, condition=0):
         super().__init__(condition)
-        self.category = "Electronics"
+        self.condition = condition
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
