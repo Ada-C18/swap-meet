fd0697d4ae56d4bcfe11b059a8c50be71a6fd298
Valentina Ruiz <v.zruiz06@gmail.com>
2022-10-05 07:35:33 -0700

    passed wave 5.1

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,10 +3,10 @@ from swap_meet.item import Item
 
 
 class Clothing(Item):
-    def __init__(self, Clothing, condition = 0):
+    def __init__(self, category = 0, condition = 0):
 
-        super().__init__(condition)
-        self.category = Clothing 
+        super().__init__(condition = 0)
+        self.category = "Clothing" 
 
 
 
