823c284cb8d99906e8d3a4deeb2f063131454ab4
Michelle Oh <ohmotrl@gmail.com>
2022-10-05 12:01:17 -0400

    addded feunction to wave5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,6 @@
+import numbers
+
+
 class Item:
     def __init__(self, condition = 0, category = ""):
         self.category = category
@@ -6,8 +9,18 @@ class Item:
     def __str__(self):
         return 'Hello World!'
 
-    # def condition_description(self):
+    def condition_description(self):
+        condition_list = ["heavily used", "moderately used", "slightly used", "used", "almost new", "brand new"]
+        
+        for i in range(len(condition_list)):
+            print(i)
+            if i == self.condition:
+                return condition_list[i]
 
+        
+        
+        
+        
+# matched condition with index
 
-    
-    
+#self.condtion get us a numbers
