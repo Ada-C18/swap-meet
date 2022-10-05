fe725f6e8d075d968d73ded598318afb109bae8d
Mia Yang <miacxyang@gmail.com>
2022-10-05 14:37:22 -0700

    Wave 3, 4, & part of Wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,4 +1,11 @@
 class Item:
     
-    def __init__(self, category = ""):
-        self.category = category
+    def __init__(self, category = None, condition = 0):
+        self.category = category if category is not None else ""
+        self.condition = condition
+
+    def __str__(self):
+        return "Hello World!"
+
+    def condition_description(self):
+        pass
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,7 @@
 class Vendor:
     
-    def __init__(self, inventory = []):
-        self.inventory = inventory
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory is not None else []
     
     def add(self, item):
         self.inventory.append(item)
@@ -20,3 +20,23 @@ class Vendor:
             if item.category == category:
                 item_list.append(item)
         return item_list
+
+    def swap_items(self, other, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other.inventory:
+            return False
+
+        self.remove(my_item)
+        other.add(my_item)
+        other.remove(their_item)
+        self.add(their_item)
+        return True
+
+    def swap_first_item(self, friend):
+        if len(self.inventory) == 0 or len(friend.inventory) == 0:
+            return False
+        
+        friend.add(self.inventory[0])
+        self.remove(self.inventory[0])
+        self.add(friend.inventory[0])
+        friend.remove(friend.inventory[0])
+        return True
