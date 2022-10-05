aaf47757bd6569d133fe0aac5586b8485eb46608
Valentina Ruiz <v.zruiz06@gmail.com>
2022-10-05 09:05:36 -0700

    wave 5 completed

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -6,7 +6,7 @@ from swap_meet.item import Item
 class Clothing(Item):
     def __init__(self, category = "Clothing", condition = 0):
 
-        super().__init__(condition)
+        super().__init__(category, condition)
         self.category = "Clothing" 
         self.condition = condition
 
