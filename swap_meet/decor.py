f9462ee084507f088906f0200a7fea065bcc9c97
Rose Le <rosehle@protonmail.com>
2022-10-05 14:21:58 -0700

    implemented wave_05 and passed unit tests

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,8 @@
-class Decor:
-    pass
+from .item import Item
+class Decor(Item):
+    def __init__(self, condition):
+        super().__init__(condition, condition)
+        self.category = "Decor"
+        
+    def __str__(self):
+        return "Something to decorate your space."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,22 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition=None):
         self.category = category
+        if condition == None:
+            self.condition = 0
+        else:
+            self.condition = condition
         
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 5:
+            return "This is probably out of your price range normally"
+        elif self.condition >= 4:
+            return "This is still pretty good, just not mint."
+        elif self.condition >= 3:
+            return "This isn't bad, could be better."
+        elif self.condition >= 2:
+            return "Not the best, you could live without it."
+        elif self.condition > 0:
+            return "Put this down and walk away."
