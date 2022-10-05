cb054864e4a412ecfe478dbf38cae5d94810ac0b
Jennifer <datateur@gmail.com>
2022-10-05 13:07:00 -0700

    Completed project

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -3,7 +3,7 @@ from .item import Item
 
 class Decor(Item):
     def __init__(self, category = "Decor", condition = 0):
-        Item.__init__(self, category, condition)
+        super().__init__(category, condition)
 
     def __str__(self):
         return "Something to decorate your space."
