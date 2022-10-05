bd3b37e3ae5149b0c513a03ceda37a5bc6a7485e
Tapasya Khatri <tapasya@Tapasyas-MacBook-Air.local>
2022-10-04 21:50:49 -0700

    Wave 5 done.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,8 +1,30 @@
 class Item:
     """creates an Item object with an attribute called 'category'."""
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
+        
     def __str__(self):
         return "Hello World!"
     
+    def condition_description(self):
+        if self.condition == 0:
+            return "Mint"
+        elif self.condition == 1:
+            return "like new"
+        elif self.condition == 2:
+            return "good/gently used"
+        elif self.condition == 3:
+            return "used/some wear and tear"
+        elif self.condition == 4:
+            return "used/as is"
+        elif self.condition == 5:
+            return "heavily used/might have broken or missing parts"
+        else:
+            raise ValueError("condition must be between 0 to 5.")
+        
+
+
+
+
 
