a7e4eabbf5f01f40e7d4a79b6d300ef5afa5ab44
Tanil <cadette.tanil@gmail.com>
2022-10-05 16:21:10 -0400

    completed wave 4 and started wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,22 @@
 class Item:
-    def __init__(self, category = ""):
+    def __init__(self, category = "", condition = 0):
         self.category = category
+        self.condition = condition
+        
+    def __str__(self):
+        return "Hello World!"
+    
+    def condition_description(self):
+        if self.condition == 0:
+            return "Horrible"
+        elif 0 < self.condition <= 1:
+            return "Used"
+        elif 1 < self.condition <= 2:
+            return "Lightly Used"
+        elif 2 < self.condition <= 3:
+            return "Good"
+        elif 3 < self.condition <= 4:
+            return "Very Good"
+        elif 4 < self.condition <= 5:
+            return "Excellent"
+            
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,7 @@
 class Vendor:
-    def __init__(self, inventory=[]):
-        self.inventory = inventory
-
+    def __init__(self, inventory=None):
+        self.inventory = inventory if inventory is not None else []
+        
     def add(self, item):
         self.inventory.append(item)
         return item
@@ -18,3 +18,23 @@ class Vendor:
             if item.category == category:
                 category_list.append(item)
         return category_list
+    
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            friend.add(my_item) 
+            self.add(their_item)
+            self.remove(my_item)
+            friend.remove(their_item)
+            
+            return True
+        else:
+            return False
+    
+    def swap_first_item(self, friend):
+        if self.inventory and friend.inventory:
+            self.swap_items(friend, self.inventory[0], friend.inventory[0])
+            return True
+        else:
+            return False
+            
+            
