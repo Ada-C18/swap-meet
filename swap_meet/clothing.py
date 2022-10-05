84dbbfe1b1236651b63e347107832d8413d0a57c
Emily <sonemily+github@gmail.com>
2022-10-05 13:55:57 -0400

    Use super init in item child classes for Wave 5.

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,10 +1,9 @@
 from swap_meet.item import Item
 
+
 class Clothing(Item):
     def __init__(self, category = "Clothing", condition = None):
-        self.category = category
-        self.condition = condition if condition is not None else 0
-
+        super().__init__(category, condition)
 
     def __str__(self):
         return "The finest clothing you could wear."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,9 +1,9 @@
 from swap_meet.item import Item
 
+
 class Electronics(Item):
     def __init__(self, category = "Electronics", condition = None):
-        self.category = category
-        self.condition = condition if condition is not None else 0
+        super().__init__(category, condition)
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
