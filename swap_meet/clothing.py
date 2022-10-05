f198ebf3be7d83bc0d86a9e3b2a75ca33c9a90f9
nina-tuyen tran <ninatuyentran@gmail.com>
2022-10-04 20:52:40 -0700

    first commit with edits on clothing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, category, condition):
+        self.category = "Clothing"
+        self.condition =  super(Item, self).condition_description(self)
+        
+        
+
+        
+    
+    def __str__(self):
+        self.Clothing = "The finest clothing you could wear."
+        return self.Clothing
