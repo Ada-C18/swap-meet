c9e8c84cd6b91cd8f4fd835c95bfbc3ad352eb2c
Milena Souto <yamadasouto@gmail.com>
2022-10-05 14:52:01 -0700

    Fix attributes and parameters in clothing item instance

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -4,9 +4,9 @@ from .item import Item
 
 # class Clothing:
 class Clothing(Item):
-    def __init__(self, category="", condition=0):
-        super().__init__(category, condition)
-        self.category = "Clothing"
+    def __init__(self, condition=0.0):  # parameters  given by user
+        super().__init__("Clothing", condition)
+        # attributes
 
     def __str__(self):
         super().__str__()
