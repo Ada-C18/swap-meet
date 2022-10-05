9fe2528c9716e01c1df8160039c3a924cf103bb9
Lee Reyes <reyeslee94@gmail.com>
2022-10-05 08:22:49 -0700

    cleaned up code

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,11 +3,7 @@ from swap_meet.item import Item
 class Clothing(Item):
     def __init__(self, category="Clothing", condition=0):
         super().__init__(category,condition)
-        # self.category = category
-        # self.condition = condition
 
     def __str__(self):
         return "The finest clothing you could wear."
 
-    def condition_description(self):
-        return super().condition_description()
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,11 +3,7 @@ from swap_meet.item import Item
 class Electronics(Item):
     def __init__(self, category="Electronics", condition=0):
        super().__init__(category,condition)
-        # self.category = category
-        # self.condition = condition
     
     def __str__(self):
         return "A gadget full of buttons and secrets."
     
-    def condition_description(self):
-        return super().condition_description()
