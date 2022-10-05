508547e4b2a79af4fb3e96ab6eb26243fbe9bb7d
Kae Sluder <kaesluder@fastmail.com>
2022-10-04 21:34:32 -0400

    working version of clothing child class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
-class Clothing:
-    pass
+from .item import Item
+
+class Clothing(Item):
+
+    # category-specific constructor using "Clothing" as the 
+    # default. 
+    def __init__(self, category="Clothing", condition=0):
+
+        # pass variables to the super().__init__ function
+        # including the default category. 
+        super().__init__(category, condition)
+
+    def __str__(self):
+        return "The finest clothing you could wear."
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,10 +1,13 @@
 class Item:
 
-    def __init__(self, category = ""):
+    # TODO: maybe we should reorder these arguments if category is 
+    # set by default in child classes?
+    def __init__(self, category = "", condition=0):
         """
         sets up the Item instance with an optional category attribute
         """
         self.category = category
+        self.condition = condition 
 
     def __str__(self):
         return "Hello World!"
