8e093e83693ae9e4433cfcea3552c129cda15521
Aviva <avivajoy.gars@gmail.com>
2022-10-05 08:34:32 -0600

    made wave 5 into a dict

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,3 +1,4 @@
+# from swap_meet.item import Item
 class Clothing:
     def __init__(self,category = "Clothing"):
         self.category = category if category is not None else ""
@@ -8,30 +9,13 @@ class Clothing:
     def __str__(self):
         return "The finest clothing you could wear."
 
-
-    # "Aviva try git push"
-    # "clothing..how hard can it be"
     def condition_description(self):
-        # self.condition_dict = condition_dict
         condition_dict= {"worst": 0, "bad": 1, "good":2, "better":3, "best":4, "great":5}
 
-        
-        for key, value in condition_dict:
-            if self.condition == condition_dict.values():
-                return condition_dict.keys()
+        for key, value in condition_dict.items():
+            if self.condition == value:
+                return key
 
-        # if self.condition == 0:
-        #     return "worst"
-        # elif self.condition == 1:
-        #     return "bad"
-        # elif self.condition == 2:
-        #     return "good"
-        # elif self.condition == 3:
-        #     return "better"
-        # elif self.condition == 4:
-        #     return "best"
-        # elif self.condition == 5:
-        #     return "great"
         
     
 
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,14 @@
 class Item:
-    def __init__(self, category = None, condition = 0.0):
+    def __init__(self, category=None, condition=0.0):
         self.category = category if category is not None else ""
         self.condition = condition
+
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        condition_dict = {"worst": 0, "bad": 1,
+                          "good": 2, "better": 3, "best": 4, "great": 5}
+        for key, value in condition_dict.items():
+            if self.condition == value:
+                return key
