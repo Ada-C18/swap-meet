c19fa5e4dbe3d79591659abcf621cf229d90e2a8
Lilian Mora <Lilianashley94@gmail.com>
2022-10-05 14:16:56 -0700

    waves1-3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,11 @@
+from unicodedata import category
+
+
 class Item:
-    pass
+    def __init__(self, category = None):
+        self.category = category if category != None else ""
+
+
+    
+    def __str__(self):
+        return f"Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,31 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory != None else []
+      
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        else:
+            return False
+
+    def get_by_category(self, category):
+        items = []
+        for item in self.inventory:
+            if item.category == category:
+                items.append(item)
+        return items
+
+    def swap_items(self, another_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in another_vendor.inventory:
+            self.add(their_item)
+            self.remove(my_item)
+            another_vendor.add(my_item)
+            another_vendor.remove(their_item)
+            return True
+        else:
+            return False
