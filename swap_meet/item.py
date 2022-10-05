3764d0fd732e0a8abcde1b03b731a69d032438bd
Annie <annie.gallagher7@gmail.com>
2022-10-05 15:10:05 -0600

    updated category in init and added condition descriptions

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,17 @@
 class Item:
-    def __init__(self, category = None):
-        self.category = category if category is not None else ""
+    def __init__(self, category = "", condition = 0):
+        self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 5:
+            return "Are you sure this is second-hand?"
+        elif 3 <= self.condition < 5:
+            return "Pretty minty, if you ask me"
+        elif 1.5 <= self.condition < 3:
+            return "Probably best to clean this one first...."
+        elif self.condition < 1.5:
+            return "Are you sure you want this?"
