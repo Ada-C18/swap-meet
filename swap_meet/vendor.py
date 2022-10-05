ef227cac64857d91ae46c1fcf8e4ca04af0edfa8
hoku mcangus <mcangusohana2@gmail.com>
2022-10-05 12:27:09 -0700

    Worked on wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,9 +1,10 @@
 class Vendor:
     
-    def __init__(self, inventory = []):
-        # if self.inventory is None:
-        #     self.inventory = []
+    def __init__(self, inventory = None):
         self.inventory = inventory
+        if not inventory:
+            self.inventory = []
+        
 
     def add(self, item):
         self.inventory.append(item)
@@ -17,3 +18,25 @@ class Vendor:
             return item
         except SyntaxError as err:
             print("Please enter a valid item")
+
+    def get_by_category(self, category):
+        category_list = []
+        for item in self.inventory:
+            if item.category == category:
+                category_list.append(item)
+        return category_list
+
+
+    # def swap_items(self, vendor, my_item, their_item):
+    #     remove my_item from self.inventory
+    #     append my_item to vendor.inventory
+
+    #     remove their_item from vendor.inventory
+    #     append their_item to self.inventory
+
+    #     returns True
+    #     if vendor.inventory not have my_item 
+    #     or vendor.inventory not have their_item
+    #     return False
+
+
