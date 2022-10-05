d69aaf471ce85245128a1e61f5165aa3ece1a30c
Anjula Tamang <parkhrinj@gmail.com>
2022-10-05 11:44:18 -0400

    started on 6

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,20 +1,25 @@
-# from swap_meet.item import Item
+from swap_meet.item import Item
 class Clothing:
-    def __init__(self,category = "Clothing"):
-        self.category = category if category is not None else ""
     def __init__(self, category=None, condition=0.0):
         self.category = "Clothing"
         self.condition = condition
+        #super().__init__(category , condition)
 
     def __str__(self):
         return "The finest clothing you could wear."
-
+    
     def condition_description(self):
-        condition_dict= {"worst": 0, "bad": 1, "good":2, "better":3, "best":4, "great":5}
-
+        condition_dict = {"worst": 0, "bad": 1,
+                          "good": 2, "better": 3, "best": 4, "great": 5}
         for key, value in condition_dict.items():
             if self.condition == value:
                 return key
+
+
+
+
+    
+
 
         
     
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,7 +1,12 @@
+from swap_meet.item import Item
+
 class Electronics:
     def __init__(self, category=None, condition=0.0):
         self.category = "Electronics"
         self.condition = condition
+        #super().__init__(category , condition)
+
+
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
