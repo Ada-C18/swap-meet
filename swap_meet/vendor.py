9b4a7dd82661659fafbaf336b943dd5e8f05787a
ashanti <ammanier2@gmail.com>
2022-10-05 15:46:18 -0400

    wave6 doc_string logic

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -89,6 +89,19 @@ class Vendor:
         return best_item
         
 
-
+# - `Vendor`s have a method named `swap_best_by_category`, 
+#       which will swap the best item of certain categories with another `Vendor`
+#   - It takes in three arguments
+#     - `other`, which represents another `Vendor` instance to trade with
+#     - `my_priority`, which represents a category that the `Vendor` wants to receive
+#     - `their_priority`, which represents a category that `other` wants to receive
+#   - The best item in my inventory that matches `their_priority` category is 
+#      swapped with the best item in `other`'s inventory that matches `my_priority`
+#     - It returns `True`
+#     - If the `Vendor` has no item that matches `their_priority` category, 
+#       swapping does not happen, and it returns `False`
+#     - If `other` has no item that matches `my_priority` category, 
+#       swapping does not happen, and it returns `False`
+    
     def swap_best_by_category(self, other_vendor, my_priority, their_priority):
         pass
