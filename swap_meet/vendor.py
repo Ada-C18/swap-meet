e9e2bb2eaef970ce98f4729450db660ef73a421e
Maggie <mjtice0@gmail.com>
2022-10-04 21:31:39 -0600

    wave four tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -12,7 +12,7 @@ class Vendor:
         self.inventory.append(item)
         return item
 
-    def remove(self, item):
+    def remove(self, item):                         
         if item in self.inventory:
             self.inventory.remove(item)
             return item
@@ -27,9 +27,64 @@ class Vendor:
         #         items_list.append(item)
         # return items_list
 
-    def different_vendor():
-        #composite/component relationship
-        pass
 
-    def swap_items(self, different_vendor(), my_item, their_item):
-        pass
+ 
+
+   
+   
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #takes in an instance of another vendor, representing friend they are swapping with
+    def swap_first_item(self, Vendor): #
+    
+        if not self.inventory:
+            return False
+        
+        # if not Vendor.inventory:
+        #     return False
+        
+      #loop through items, remove item 0, append it to friends list 
+      #self.inventory.remove()
+      #self.inventory.add()
+
+      #loop for appending items?
+        for item in self.inventory:
+            #self.inventory.remove(0)
+            self.inventory.pop(0)
+            Vendor.inventory.append(0)
+     # loop through friend inventory, remove item 0, append to self inventory list      
+    
+    # short hand way to do it --  Vendor.inventory.append(self.inventory.pop(0))
+
+            
+    
+
+
