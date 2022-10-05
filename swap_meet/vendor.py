a0462bf34486789c3b2e18d70db04b544621200a
Emma Kizil <fmodabber@outlook.com>
2022-10-04 20:22:10 -0700

    w3 & w4 done

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,6 @@
 from nis import cat
 from .item import Item
+
 class Vendor:
     def __init__(self, inventory=None) -> None:
         if inventory is None:
@@ -22,3 +23,20 @@ class Vendor:
             if item.category == category:
                 items_list.append(item)
         return items_list
+
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            friend.add(my_item)
+            self.remove(my_item)
+            self.add(their_item)
+            self.remove(their_item)
+            return True
+        return False
+
+    def swap_first_item(self, friend):
+        if not self.inventory or not friend.inventory:
+            return False
+        else:
+            self.add(friend.inventory.pop(0))
+            friend.add(self.inventory.pop(0))
+            return True
