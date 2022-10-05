af2fe6d7732e268329d1e10c079fb9c55341f758
Maria Fernanda Perez Rojo <mariafperez603@gmail.com>
2022-10-05 10:52:57 -0400

    Wave 5 complete

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -4,27 +4,23 @@
 
 
 class Item:
-<<<<<<< HEAD
     def __init__(self, category=None, condition = 0 ):
-=======
-    def __init__(self, category=None, condition=0):
->>>>>>> 3bf8151819973e065f0f42cddd6430e0f0a0cbb7
         self.category = category if category is not None else ""
         self.condition = condition if condition is not None else 0
     
     def __str__(self):
         return "Hello World!"
 
-<<<<<<< HEAD
+    def condition_description(self):
+        if 0 <= self.condition < 2:
+            return "Hard Pass!"
+        elif 2 <= self.condition < 3:
+            return "Its seen better days!"
+        elif 3 <= self.condition < 4:
+            return " A little loved"
+        elif 4 <= self.condition < 5:
+            return "Slighty used"
+        elif self.condition == 5:
+            return "Mint"
         
-=======
-    def condition_description(self, condition):
         
-# - All three classes and the `Item` class have an instance method named `condition_description`, 
-# which should describe the condition in words based on the value, assuming they all range from 0 to 5. 
-# These can be basic descriptions 
-# (eg. 'mint', 'heavily used') but feel free to have fun with these (e.g. 'You probably want a glove for this one..."). 
-# The one requirement is that the `condition_description` for all three classes above have the same behavior.
-
-        
->>>>>>> 3bf8151819973e065f0f42cddd6430e0f0a0cbb7
