1965ea01c9ad7326eddae3155124f34f7fb5fff9
Abby <abbychoi97@gmail.com>
2022-10-05 14:15:33 -0700

    added doc strings and refactors condition_description function

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,5 +1,6 @@
 class Item:
     def __init__(self, category="", condition=0.0, age=0):
+        '''takes in a category string, condition float, and age to instantiate an instance of Item'''
         self.category = category
         self.condition = condition
         self.age = age
@@ -8,13 +9,23 @@ class Item:
         return "Hello World!"
     
     def condition_description(self):
-        if self.condition == 0: 
-            return "Condition is unknown"
-        elif 1 <= self.condition < 3:
-            return "It needs a lot of love"
-        elif self.condition == 3:
-            return "It just needs a little love"
-        elif self.condition == 4: 
-            return  "Very good"
-        elif self.condition == 5:
-            return "Like New!"
+        '''returns a description of the item condition'''
+        condition = {
+            0:"Condition is unknown", 
+            1: "It needs lots of love",
+            2: "It needs some love",
+            3: "It just needs a little love",
+            4: "Very good",
+            5: "Like New!"
+            }
+        return condition[self.condition]
+        # if self.condition == 0: 
+        #     return "Condition is unknown"
+        # elif 1 <= self.condition < 3:
+        #     return "It needs a lot of love"
+        # elif self.condition == 3:
+        #     return "It just needs a little love"
+        # elif self.condition == 4: 
+        #     return  "Very good"
+        # elif self.condition == 5:
+        #     return "Like New!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,16 +1,32 @@
 
 class Vendor:
     def __init__(self, inventory=None):
+        '''
+        takes in an inventory list of items and will create an instance of Vendor
+        '''
         if not inventory:
             self.inventory = []
         else:
             self.inventory = inventory
     
     def add(self, item):
+        '''
+        Input: item
+        Returns item
+
+        Will take in an item to add to vendor's inventory
+        '''
         self.inventory.append(item)
         return item
     
     def remove(self, item):
+        '''
+        Input: item
+        Returns item or False
+
+        Will take in an item to remove from vendor's inventory
+        If item is not in vendor's inventory, will return False
+        '''
         if item not in self.inventory:
             return False
         else:
@@ -18,6 +34,13 @@ class Vendor:
             return item
     
     def get_by_category(self, category): 
+        '''
+        Input: category
+        Return list of items in that category
+
+        Will take in a category and will return a list of items within that category
+        If no items in the category, will return False
+        '''
         items = []
         for item in self.inventory:
             if item.category == category:
@@ -25,6 +48,12 @@ class Vendor:
         return items
 
     def swap_items(self, vendor, my_item, their_item):
+        '''
+        Input: other_vendor, my_item, their_item
+        Return True if able to swap, False if not able to swap 
+
+        Will swap my_item from vendor instance calling the function with their_item from vendor inputted.
+        '''
         if my_item not in self.inventory or their_item not in vendor.inventory:
             return False
         else:
@@ -37,6 +66,13 @@ class Vendor:
             return True
 
     def swap_first_item(self, vendor):
+        '''
+        Input: other_vendor, my_item, their_item
+        Return True if able to swap, False if not able to swap 
+
+        Will swap first item from inventory of vendor instance calling the function,
+        with the first item from inventory of vendor inputted.
+        '''
         if self.inventory and vendor.inventory:
             self.swap_items(vendor, self.inventory[0], vendor.inventory[0])
             # self.add(vendor.remove(vendor.inventory[0]))
@@ -45,6 +81,11 @@ class Vendor:
         return False
         
     def get_best_by_category(self, category):
+        '''
+        Input: Category 
+        Return: best condition item 
+        will take in a category and return the item with the best condition
+        '''
         best_condition = -1
         best_item = None
         
@@ -56,6 +97,11 @@ class Vendor:
         return best_item
     
     def swap_best_by_category(self, other, my_priority, their_priority):
+        '''
+        Input: other vendor, my priority category, their priority category
+        Return: True if successfully swapped, False if unsuccessful 
+        will swap best condition item in respective parties desired category
+        '''
         if not self.get_by_category(their_priority) or not other.get_by_category(my_priority):
             return False
         else:
@@ -63,6 +109,10 @@ class Vendor:
             return True    
     
     def get_newest(self):
+        '''
+        Return newest item or False if inventory is empty 
+        will find the newest item in the inventory
+        '''
         if len(self.inventory) > 0:
             newest = self.inventory[0]
             youngest_age = self.inventory[0].age
@@ -74,10 +124,14 @@ class Vendor:
         return False
 
     def swap_by_newest(self, other):
+        '''
+        Return newest item or False if inventory is empty 
+        will swap the newest items in each vendor's inventory
+        '''
         if self.inventory and other.inventory:
-            self_newest = self.get_newest()
-            other_newest = other.get_newest()
-            return self.swap_items(other, self_newest, other_newest)
+            # self_newest = self.get_newest()
+            # other_newest = other.get_newest()
+            return self.swap_items(other, self.get_newest(), other.get_newest())
         return False
         
 
