b2ce5f3f62bdf382499c26328e814c7bd48977e9
Aria Stroud <stroudaria@gmail.com>
2022-10-05 13:45:48 -0700

    Adds solution for test wave 5

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,22 @@
 class Item:
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0):
         self.category = category
+        self.condition = condition
 
     # __str__ is called when str() function is invoked on an object
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition == 0:
+            return "Worn down"
+        elif self.condition == 1:
+            return "Heavily used"
+        elif self.condition == 2:
+            return "Gently used"
+        elif self.condition == 3:
+            return "Solid"
+        elif self.condition == 4:
+            return "Practically perfect"
+        elif self.condition == 5:
+            return "Never worn before"
