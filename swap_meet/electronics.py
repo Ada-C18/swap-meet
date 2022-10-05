6df522c8f5c10d4ac526202404ea240739a4e795
dainiz <dainiz.almazan@gmail.com>
2022-10-05 13:50:35 -0700

    Fixes missing category bug in Electronics class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,9 @@
-from typing import ItemsView
 from swap_meet.item import Item
+
 class Electronics(Item):
     def __init__(self,category="Electronics", condition="0"):
         super().__init__(condition="0")
+        self.category = category
 
     def __str__(self) -> str:
         return "A gadget full of buttons and secrets."
