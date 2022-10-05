aa872e044b6018b184e9df206d0b921cc7210857
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-05 12:15:27 -0700

    refactored new classes electronics, clothing, decor to include super()

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,18 +1,5 @@
 from .item import Item
 
-# class Decor(Item):
-#     def __init__(self, category="Decor", condition=0):
-#         self.category = category
-#         self.condition = condition
-
-# DISCUSS WITH GALINA - inheriting from Item and then overriding category?
-# class Decor(Item):
-#     def __init__(self, category="Decor", condition=0):
-#         Item.__init__(self, category, condition)
-#         self.category = category
-
-# DISCUSS WITH GALINA - why can I not just inherit condition? When you call a superclass you can leave off "self"
-#you have to say it again because if not, the child's lack of default would override the super's default -- when Python is looking at what to take in as an argument, it wouldn't look at the super at all (until super().__init__ is called). So, you would get an error if you didn't pass in a value for condition when instantiating an apple
 class Decor(Item):
     def __init__(self, condition=0):
         super().__init__(category="Decor", condition=condition)
