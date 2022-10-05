dffa705db5f10af6b7cbee01d61deaa3d95e7912
Kae Sluder <kaesluder@fastmail.com>
2022-10-05 16:34:45 -0400

    item condition description

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,13 @@
+DESCRIPTION_DICT = {
+    0: "Did you even check?",
+    1: "Possibly good for parts, possibly.",
+    2: "A good holiday gift for your least favorite relative. ",
+    3: "Not bad...",
+    4: "Worth the price of admission.",
+    5: "A hidden treasure for Antiques Roadshow.",
+}
+
+
 class Item:
 
     # TODO: maybe we should reorder these arguments if category is 
@@ -14,4 +24,14 @@ class Item:
         return "Hello World!"
 
     def condition_description(self):
-        
+        """
+        Return a string description based on self.condition.
+        """
+
+        if self.condition < 0 or self.condition > 5:
+            return "Did you even read the instructions?"
+            
+        rounded_score = round(self.condition)
+        return DESCRIPTION_DICT[rounded_score]
+
+
