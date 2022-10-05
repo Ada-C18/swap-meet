69117c9139adbf72e0dc1cd753b1dc5bf05503f1
Michelle Oh <ohmotrl@gmail.com>
2022-10-05 10:24:12 -0400

    merge conflict

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,4 +1,19 @@
 from swap_meet.item import Item
 
-class Clothing:
-    print("Hello, World!")
+# class Clothing(Item):
+#     def __init__(self, condition = None):
+        
+#         super().__init__(condition, category = "Clothing") # default arg has to be at the end
+            
+#     def __str__(self): 
+#         # print([cloth.category for cloth in self.inventory])
+#         return 'The finest clothing you could wear.'
+    
+class Clothing(Item):
+    def __init__(self):
+        
+        super().__init__(category = "Clothing")
+        
+    def __str__(self): 
+        # print([cloth.category for cloth in self.inventory])
+        return 'The finest clothing you could wear.'
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,4 +3,8 @@ from swap_meet.item import Item
 class Electronics:
     # might need Super() in __init__
     # override the __str__ method
-    pass
+    def __init__(self):
+        pass
+        
+    def __str__(self):
+        return 'A gadget full of buttons and secrets.'
