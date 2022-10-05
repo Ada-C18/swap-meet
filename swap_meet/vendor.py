04aa7587290af59963a1d5816c42f351b4ec07f4
hang tran <hangmtran101@gmail.com>
2022-10-05 08:10:23 -0700

    wave 4 and 5

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,6 +1,8 @@
 from swap_meet.item import Item
 class Vendor:
-    def __init__(self, inventory = []):
+    def __init__(self, inventory = None):
+        if inventory is None:
+            inventory = []
         self.inventory = inventory
         
         
@@ -38,9 +40,22 @@ class Vendor:
             return True
 
 
+    def swap_first_item(self, another_vendor):
+        if self.inventory == [] or another_vendor.inventory == []:
+            return False
+        else:
+            my_first_item = self.inventory[0]
+            their_first_item = another_vendor.inventory[0]
+            return self.swap_items(another_vendor, my_first_item, their_first_item)
+            
 
-    
 
+
+   
+            
+   
+            
+            
 
 vendor = Vendor()
 
@@ -75,7 +90,7 @@ jolie = Vendor(
     )
 
 
-
+ 
 
 
 
