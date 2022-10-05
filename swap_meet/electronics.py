87db9e44d49955ede47b8c2d6507df265206bec1
dainiz <dainiz.almazan@gmail.com>
2022-10-05 13:44:35 -0700

    Creates parent/child relationship between Item and Electronics

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,7 +1,8 @@
+from typing import ItemsView
 from swap_meet.item import Item
-class Electronics:
-    def __init__(self,category, condition = '0'):
-        self.category = "Electronics"
+class Electronics(Item):
+    def __init__(self,category="Electronics", condition="0"):
+        super().__init__(condition="0")
 
     def __str__(self) -> str:
         return "A gadget full of buttons and secrets."
