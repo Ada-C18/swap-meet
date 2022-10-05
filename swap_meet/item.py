f17ba74074ada7d6cf4161e7a334735cfa2cecf3
dainiz <dainiz.almazan@gmail.com>
2022-10-05 13:31:42 -0700

    Adds condition attribute and condition_description() method to Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,7 +1,19 @@
 class Item:
 
-    def __init__(self, category=""):
+    def __init__(self, category="", condition=0,):
         self.category = category
+        self.condition = condition
 
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        conditions = {
+            0: "horrible",
+            1: "heavily used",
+            2: "OK",
+            3: "slightly used",
+            4: "like new",
+            5: "mint",
+        }
+        return conditions[int(self.condition)]
