5879aa57e8acd3c61bfce763928749c11c85219a
Aviva <avivajoy.gars@gmail.com>
2022-10-05 09:48:54 -0600

    merge conflict

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from swap_meet.item import Item
-=======
-from swap_meet.item import Item 
->>>>>>> d69aaf471ce85245128a1e61f5165aa3ece1a30c
 class Decor:
     def __init__(self, category=None, condition=0):
         self.category = "Decor"
