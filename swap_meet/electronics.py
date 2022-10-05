194da2c9cf809a819bd0f91ea1e02f4486326139
Tanil <cadette.tanil@gmail.com>
2022-10-05 17:15:59 -0400

    finishing wave 6

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,9 @@
 from swap_meet.item import Item
 
 class Electronics(Item):
-    def __init__(self, category, condition):
-        super().__init__(category = "Electronics", condition = 0)
+    def __init__(self, category = "Electronics", condition = 0):
+        super().__init__(category = "Electronics")
+        self.condition = condition
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
