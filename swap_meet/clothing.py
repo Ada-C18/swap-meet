a89db2f29ac71c6cae9e71b8430fdcc035ee4d89
Farrah <farrah.unger24@gmail.com>
2022-10-05 14:18:02 -0400

    wave 5 done

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,12 +1,11 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, condition = None):
+    def __init__(self, condition = 0):
         super().__init__(condition, category = "Clothing")
     
 
     def __str__(self):
         return "The finest clothing you could wear."
 
-    def condition_description(self):
-        pass
+    
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -3,8 +3,8 @@ import numbers
 
 class Item:
     def __init__(self, condition = 0, category = ""):
-        self.category = category
-        self.condition = condition
+        self.condition = condition if condition is not None else 0
+        self.category = category if category is not None else ""
     
     def __str__(self):
         return 'Hello World!'
@@ -12,10 +12,10 @@ class Item:
     def condition_description(self):
         condition_list = ["heavily used", "moderately used", "slightly used", "used", "almost new", "brand new"]
         
-        for i in range(len(condition_list)):
-            print(i)
+        for i in range(len(condition_list) + 1):
+            # print(i)
             if i == self.condition:
-                return condition_list[i]
+                return condition_list[i - 1]
 
         
         
