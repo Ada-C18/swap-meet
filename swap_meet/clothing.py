716f75e183aca990f5b2c011977d1b31136e0ee4
dainiz <dainiz.almazan@gmail.com>
2022-10-05 14:35:54 -0700

    Fixes bugs in Item, Decor, Electronics, and Clothing classes

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,9 +1,8 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, category="Clothing", condition="0"):
-        super().__init__(condition="0")
-        self.category = category
+    def __init__(self, condition=0):
+        super().__init__(condition, category="Clothing")
 
     def __str__(self):
         return "The finest clothing you could wear."
--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,9 +1,8 @@
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, category="Decor", condition="0"):
-        super().__init__(condition="0")
-        self.category = category
+    def __init__(self, condition=0):
+        super().__init__(condition, category="Decor")
 
     def __str__(self):
         return "Something to decorate your space."
