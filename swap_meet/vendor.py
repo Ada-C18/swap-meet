f1ede6a1e9d1e363fd7644d4448424345c85bfd4
Tiffany Tran <tif.tran86@gmail.com>
2022-10-05 20:27:33 -0700

    DRY'd the code and all tests passed!

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -29,8 +29,9 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
-        self.remove(my_item)
+
         other_vendor.add(my_item)
+        self.remove(my_item)
         self.add(their_item)
         other_vendor.remove(their_item)
         return True
@@ -38,11 +39,15 @@ class Vendor:
     def swap_first_item(self, other_vendor):
         if not other_vendor.inventory or not self.inventory:
             return False
-        other_vendor.add(self.inventory[0])
-        self.remove(self.inventory[0])
-        self.add(other_vendor.inventory[0])
-        other_vendor.remove(other_vendor.inventory[0])
+        
+        self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
         return True
+        
+        # other_vendor.add(self.inventory[0])
+        # self.remove(self.inventory[0])
+        # self.add(other_vendor.inventory[0])
+        # other_vendor.remove(other_vendor.inventory[0])
+        # return True
 
     def get_best_by_category(self, category):
         best_item_list = self.get_by_category(category)
@@ -52,28 +57,33 @@ class Vendor:
         return best_item 
 
     def swap_best_by_category(self, other, my_priority, their_priority):
+
         # if my_priority not in other.inventory or their_priority not in self.category:
         #     return False
-        check_my_list = []
-        check_their_list = []
-        for item in self.inventory:
-            if item.category == their_priority:
-                check_my_list.append(True)
-            else:
-                check_my_list.append(False)
+        # check_my_list = []
+        # check_their_list = []
+        # for item in self.inventory:
+        #     if item.category == their_priority:
+        #         check_my_list.append(True)
+        #     else:
+        #         check_my_list.append(False)
         
-        for item in other.inventory:
-            if item.category == my_priority:
-                check_their_list.append(True)
-            else:
-                check_their_list.append(False)
+        # for item in other.inventory:
+        #     if item.category == my_priority:
+        #         check_their_list.append(True)
+        #     else:
+        #         check_their_list.append(False)
         
-        if not any(check_my_list) or not any(check_their_list):
+        # if not any(check_my_list) or not any(check_their_list):
+        #     return False
+        # else:
+
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other.get_best_by_category(my_priority)
+
+        if my_best_item is None or their_best_item is None:
             return False
-        else:
-            my_best_item = self.get_best_by_category(their_priority)
-            their_best_item = other.get_best_by_category(my_priority)
-            self.swap_items(other, my_best_item, their_best_item)
-            return True
+        self.swap_items(other, my_best_item, their_best_item)
+        return True
 
         
