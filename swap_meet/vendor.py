023711729748984116819db95a53023519e3a7f2
Cintia <shamsucintia@gmail.com>
2022-10-06 01:21:14 -0400

    Added parts of wave 2 + 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,4 +1,5 @@
-'''### Wave 1
+'''
+WAVE 1
 
 In Wave 1 we will create the `Vendor` class.
 
@@ -15,7 +16,8 @@ In Wave 1 we will create the `Vendor` class.
 - Similarly, every instance of `Vendor` has an instance method named `remove`, which takes in one item
 - This method removes the matching item from the `inventory`
 - This method returns the item that was removed
-- If there is no matching item in the `inventory`, the method should return `False`'''
+- If there is no matching item in the `inventory`, the method should return `False`
+'''
 class Vendor:
     def __init__(self, inventory=None):
         self.inventory = inventory if inventory is not None else []
@@ -30,3 +32,42 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+    
+'''
+WAVE 3 
+Instances of `Vendor` have an instance method named `swap_items`
+It takes 3 arguments:
+1. an instance of another `Vendor`, representing the friend that the vendor is swapping with
+2. an instance of an `Item` (`my_item`), representing the item this `Vendor` instance plans to give
+3. an instance of an `Item` (`their_item`), representing the item the friend `Vendor` plans to give
+- It removes the `my_item` from this `Vendor`'s inventory, and adds it to the friend's inventory
+- It removes the `their_item` from the other `Vendor`'s inventory, and adds it to this `Vendor`'s inventory
+- It returns `True`
+- If this `Vendor`'s inventory doesn't contain `my_item` or the friend's inventory doesn't contain `their_item`, the method returns `False`
+'''
+    # def swap_items(self, vendor, my_item, their_item):
+    #     if my_item in own inventory and their_item in friend inventory:
+    #       removes my_item from own inventory, adds to friend inventory
+    #       removes their_item from friend inventory, adds to own
+    #       return True
+    #     else: (if I or they don't have an item)
+    #       return False
+
+'''
+WAVE 4
+
+- Instances of `Vendor` have an instance method named `swap_first_item`
+- It takes one argument: an instance of another `Vendor`, representing the friend that the vendor is swapping with
+- This method considers the first item in the instance's `inventory`, and the first item in the friend's `inventory`
+- It removes the first item from its `inventory`, and adds the friend's first item
+- It removes the first item from the friend's `inventory`, and adds the instances first item
+- It returns `True`
+- If either itself or the friend have an empty `inventory`, the method returns `False`
+'''
+    # def swap_first_item(self, vendor):
+    #   If own inventory or friends inventory is not empty:
+    #       removes first item from own inventory + adds friends first item
+    #       removes first item from friends inventory + adds own first item
+    #       return True
+    #   else:
+    #       return False
--- tests/unit_tests/test_wave_02.py
+++ tests/unit_tests/test_wave_02.py
@@ -2,7 +2,7 @@ import pytest
 from swap_meet.vendor import Vendor
 from swap_meet.item import Item
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_items_have_blank_default_category():
     item = Item()
     assert item.category == ""
@@ -12,9 +12,7 @@ def test_get_items_by_category():
     item_a = Item(category="clothing")
     item_b = Item(category="electronics")
     item_c = Item(category="clothing")
-    vendor = Vendor(
-        inventory=[item_a, item_b, item_c]
-    )
+    vendor = Vendor(inventory=[item_a, item_b, item_c])
 
     items = vendor.get_by_category("clothing")
 
