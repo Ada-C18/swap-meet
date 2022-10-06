d7ed50eef1901c8a84020aaae7c40c92b2b4a0fe
misha-joy <mishellajoy@gmail.com>
2022-10-06 12:43:46 -0700

    add get best category function

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,5 +1,3 @@
-from pickle import FALSE
-
 class Vendor:
     def __init__(self, inventory=[]):
         self.inventory = inventory 
@@ -14,12 +12,6 @@ class Vendor:
         else:
             self.inventory.remove(removed_item)
             return removed_item  
-
-    def get_by_category(self, item_category):
-        self.category =  item_category 
-        items_in_category = []
-        if item.category == self.category:
-            items_in_category.append(item.category)
 
     def get_by_category(self, category=""):
         items = []
@@ -48,3 +40,30 @@ class Vendor:
             self.inventory[0] = item_2
             Vendor.inventory[0] = item_1
             return True
+
+### Wave 6
+
+# In Wave 6 we will write two methods, `get_best_by_category` and `swap_best_by_category`.
+
+# - `Vendor`s have a method named `get_best_by_category`, which will get the item with the 
+# best condition in a certain category
+#   - It takes one argument: a string that represents a category
+#   - This method looks through the instance's `inventory` for the item with the highest 
+# `condition` and matching `category`
+#     - It returns this item
+#     - If there are no items in the `inventory` that match the category, it returns `None`
+#     - It returns a single item even if there are duplicates (two or more of the same item
+#  with the same condition)
+    def get_best_by_category(self, category):
+        items = self.get_by_category(category)
+        if len(items) == 0:
+            return None
+        best_item = ''
+        best_condition = 0
+        for item in items:
+            if item.condition > best_condition:
+                best_item = item 
+                best_condition = item.condition 
+        return best_item
+            
+
