b3b4e020933c8a3adb7c5007873d636d4aaa8764
ivystrayed <ivystrayed@gmail.com>
2022-10-05 18:54:21 -0700

    added condition atribute, finally hit the compsci nightmare of i got it to work but i have no idea why, why do I have to define a default value in both the child class and parent class?

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,7 +1,8 @@
 from .item import Item
 class Electronics(Item):
-    def __init__(self):
+    def __init__(self, category, condition):
         self.category = "Electronics"
+        super().__init__(category, condition)
     def __str__(self):
         return "A gadget full of buttons and secrets."
 
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,9 +1,16 @@
 class Item:
 
-    def __init__(self, category = ""):
-        self.category = category 
+    def __init__(self, category = "",condition = 0.0):
+        self.category = category
+        self.condition = condition
    
     def __str__(self):
         return "Hello World!"
+
+    def condition_description(self):
+        if self.condition > 3.0:
+            return"its pretty good"
+        else:
+            return "ehhhh nah"
 
     
