8f4feeb1886c337c2b7bcc911ca1c9ed7ea0ce35
Eve Stroud <evestroud89@gmail.com>
2022-10-06 08:14:30 -0400

    Remove duplicate implementation of get_by_category

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -17,13 +17,6 @@ class Vendor:
             return item
         return False
 
-    def get_by_category(self, category):
-        result_item = []
-        for item in self.inventory:
-            if item.category == category:
-                result_item.append(item)
-        return result_item
-
     def swap_items(self, other_vendor, my_item, their_item):
         if my_item not in self.inventory or their_item not in other_vendor.inventory:
             return False
