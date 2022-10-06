25a7dc676134e52873f9c54d9a2c363a316232ce
Lynn <lynn@jansheski.com>
2022-10-06 07:04:05 -0700

    refactored clothing, decor and electronics classes to include super init constructor

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -3,12 +3,11 @@ from swap_meet.item import Item
 class Clothing(Item):
     
     def __init__(self, condition=0):
+        super().__init__(category="", condition=0)
         self.category = "Clothing"
         self.condition = condition
 
     def __str__(self):
         return "The finest clothing you could wear."
 
-    def condition_description(self):
-        return super().condition_description()
 
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -3,11 +3,10 @@ from swap_meet.item import Item
 class Electronics(Item):
     
     def __init__(self, condition=0):
+        super().__init__(category="", condition=0)
         self.category = "Electronics"
         self.condition = condition
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
 
-    def condition_description(self):
-        return super().condition_description()
