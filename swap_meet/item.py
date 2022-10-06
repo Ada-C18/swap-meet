b13af8a3bf8c34ed73c95319f2f92d93b94f1be8
Jan li <bingox29@gmail.com>
2022-10-06 10:10:32 -0400

    finished all waves & optional

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,23 @@
+# In Wave 2 we will create the Item class and the get_by_category method.
+# There is a module (file) named item.py inside of the swap_meet package (folder)
+# Inside this module, there is a class named Item
+# Each Item will have an attribute named category, which is an empty string by default
+# When we initialize an instance of Item, we can optionally pass in a string with the keyword argument category
+# 
 class Item:
-    pass
+    def __init__(self, category = None, condition = None, age=None):
+        self.category = category if category is not None else ""
+        self.condition = condition if condition is not None else 0
+        self.age = age if age is not None else 0
+
+    def __str__(self):
+        return "Hello World!"
+    def condition_description(self):
+        condition = {
+            0:"bad shape",
+            1:"very used",
+            2:"acceptable",
+            3:"good condition",
+            4:"great condition",
+            5:"minted"}
+        return condition[self.condition]
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,61 @@
+
 class Vendor:
-    pass
+    def __init__(self, inventory = None):
+        self.inventory = inventory if inventory is not None else []
+    
+    def add(self,item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self,item):
+        if item not in self.inventory:
+            return False
+        self.inventory.remove(item)
+        return item
+#-------------Wave2--------------------------- 
+    def get_by_category(self,category_name):
+        list =[item for item in self.inventory if item.category == category_name]
+        return list
+#-------------Wave3--------------------------- 
+    def swap_items(self, friend, my_item, their_item):
+        if my_item in self.inventory and their_item in friend.inventory:
+            friend.add(my_item)
+            self.remove(my_item)
+            friend.remove(their_item)
+            self.add(their_item)
+            return True
+        return False
+#-------------Wave4--------------------------- 
+    def swap_first_item(self,friend):
+        if not self.inventory or not friend.inventory:
+            return False
+        self.swap_items(friend,self.inventory[0],friend.inventory[0])
+        # friend.inventory.append(self.inventory[0])
+        # self.inventory.append(friend.inventory[0])
+        # self.inventory.pop(0)
+        # friend.inventory.pop(0)
+        return True
+#-------------Wave6--------------------------- 
+    def get_best_by_category(self,category_name):
+        if not self.get_by_category(category_name):
+            return None
+        return max(self.get_by_category(category_name), key =lambda item:item.condition) 
+        
+    def swap_best_by_category(self, other, my_priority, their_priority):
+        their_best=other.get_best_by_category(my_priority)
+        my_best=self.get_best_by_category(their_priority)
+        
+        if not self.get_by_category(their_priority) or not other.get_by_category(my_priority):
+            return False
+        
+        self.swap_items(other, my_best, their_best)
+        return True
+#-------------optional---------------------------
+    
+    def swap_by_newest(self, other):
+        my_newest=min(self.inventory, key =lambda item:item.age) 
+        their_newest=min(other.inventory, key =lambda item:item.age) 
+        #if it's the same age, no swap
+
+        self.swap_items(other,my_newest,their_newest)
+        return True
