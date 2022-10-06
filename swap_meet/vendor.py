95ca9e69c133539e943fe31d3110ca404dbe7c80
ivystrayed <ivystrayed@gmail.com>
2022-10-06 13:33:08 -0700

    finished swap best category method and wave 6 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+from cmath import exp
 from multiprocessing.sharedctypes import Value
 from swap_meet import item
 
@@ -32,28 +33,18 @@ class Vendor:
 
         return filter 
         
-    def swap_items(self, other_vendor, item_a, item_b):
+    def swap_items(self, other_vendor, self_item, other_item):
         
-        # try:
-        #     index_1 = self.inventory.index(item_a)
-        #     index_2 = other_vendor.inventory.index(item_b)
-        # except(ValueError):
-        #     return False 
-        
-        # self.inventory.append(other_vendor.inventory.pop(index_2))
-        # other_vendor.inventory.append(self.inventory.pop(index_1))
-        # return True
-
-        if item_a in self.inventory and item_b in other_vendor.inventory:
-        
-            self.inventory.remove(item_a)
-            self.inventory.append(item_b)
-
-            other_vendor.inventory.remove(item_b)
-            other_vendor.inventory.append(item_a)
-            return True
-        else:
+        try:
+            index_1 = self.inventory.index(self_item)
+            index_2 = other_vendor.inventory.index(other_item)
+        except(ValueError):
             return False 
+        
+        self.inventory.append(other_vendor.inventory.pop(index_2))
+        other_vendor.inventory.append(self.inventory.pop(index_1))
+        return True
+
             
     def swap_first_item(self, other_vendor):
         try:
@@ -61,3 +52,25 @@ class Vendor:
             return True
         except(IndexError):
             return False
+
+    def get_best_by_category(self, category):
+        try:
+            return max(self.get_by_category(category), key=lambda i: i.condition)
+        except(ValueError):
+            return None
+
+    def swap_best_by_category(self, other, my_priority, their_priority):
+
+        trade_happened = self.swap_items(other, self.get_best_by_category(their_priority),
+        other.get_best_by_category(my_priority))
+
+        return trade_happened
+        
+
+
+
+
+        
+
+
+        
