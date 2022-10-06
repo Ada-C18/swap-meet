c49fc82664f4baf2882e89c8f9738ca4339a179c
Milena Souto <yamadasouto@gmail.com>
2022-10-06 14:11:15 -0700

    Write asserts for wave 6 test_swap_best_by_category_reordered

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -99,7 +99,7 @@ def test_swap_best_by_category():
     assert item_f not in jesse.inventory
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_reordered():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -123,17 +123,26 @@ def test_swap_best_by_category_reordered():
         their_priority="Decor"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    # raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
     # Assertions should check:
     # - That result is truthy
+    assert result
     # - That tai and jesse's inventories are the correct length
+    assert len(jesse.inventory) == 3
+    assert len(tai.inventory) == 3
+
     # - That all the correct items are in tai and jesse's inventories, and that the items that were swapped are not there
+    assert item_c in jesse.inventory
+    assert item_c not in tai.inventory
+
+    assert item_f in tai.inventory
+    assert item_f not in jesse.inventory
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_inventory_is_false():
     tai = Vendor(
         inventory=[]
@@ -160,7 +169,7 @@ def test_swap_best_by_category_no_inventory_is_false():
     assert item_c in jesse.inventory
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_other_inventory_is_false():
     item_a = Clothing(condition=2.0)
     item_b = Decor(condition=4.0)
