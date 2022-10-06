a6bf203581bd78b225e43b3eb374d267ff3483c1
Andrea <andreagalindo.guerra@gmail.com>
2022-10-06 10:01:39 -0400

    Added docstrings to vendor.py methods

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -17,13 +17,24 @@ class Vendor:
 
 #~~~~~~~~~~~~~~~~~~~~~~~ WAVE 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def get_by_category(self, category):
+        """
+        Takes in a category as a parameter and returns a list of all items in that category.
+        """
         items = [item for item in self.inventory if item.category == category]
         return items
     
         
     def swap_items(self, another_vendor, my_item, their_item):
+        """
+        Takes in three parameters: another_vendor (an instance of Vendor), my_item (an instance of Item), 
+        and their_item (an instance of Item). 
+        If my_item is available in self.inventory and their item is available in another_vendor.inventory, 
+        swaps my_item and their_item between inventories and returns True, otherwise returns False.
+        """
+
         if my_item not in self.inventory or their_item not in another_vendor.inventory:
             return False
+
         self.inventory.remove(my_item)
         another_vendor.inventory.append(my_item)
         another_vendor.inventory.remove(their_item)
@@ -32,6 +43,10 @@ class Vendor:
         return True
 
     def swap_first_item(self, another_vendor):
+        """
+        Swaps first item between self.inventory and another_vendor.inventory. If no items in either inventory, returns False. 
+        """
+
         if not self.inventory or not another_vendor.inventory:
             return False
         self.inventory[0], another_vendor.inventory[0] = another_vendor.inventory[0], self.inventory[0]
@@ -39,6 +54,11 @@ class Vendor:
 
 #~~~~~~~~~~~~~~~~~~~~~~~ WAVE 6 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def get_best_by_category(self, category):
+        """
+        Takes in one parameter categor and returns the item with the highest condition rating. 
+        If no item within that category, returns None.
+        """
+
         list_items_by_category = self.get_by_category(category)
         if list_items_by_category:
             best_by_category = max(list_items_by_category, key=attrgetter('condition'))
@@ -47,6 +67,13 @@ class Vendor:
 
 
     def swap_best_by_category(self, other, my_priority, their_priority):
+        """
+        Takes in three parameters: other (an instance of vendor), my_priority (a string specifying a category), 
+        their_priority (a string specifying a category). Instance method get_best_by_category returns 
+        item with highest condition rating in self.inventory and other.inventory. 
+        If both items exist, they get swapped between inventories.
+        """
+        
         their_item = other.get_best_by_category(my_priority)
         my_item = self.get_best_by_category(their_priority)
         
