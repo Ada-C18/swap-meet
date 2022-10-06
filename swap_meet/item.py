99fb8ce26e9008c68a3e0f2145a6d75f80c8a92c
Emily <sonemily+github@gmail.com>
2022-10-06 16:39:29 -0400

    Refactor condition_description to use a list.

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -8,15 +8,12 @@ class Item:
         return "Hello World!"
 
     def condition_description(self):
-        if self.condition == 0:
-            return "0 flaws: the best you're going to get"
-        if 1 >= self.condition > 0:
-            return "1 flaw: you're never going to financially recover from this"
-        if 2 >= self.condition > 1:
-            return "2 flaws: you might have buyer's remorse"
-        if 3 >= self.condition > 2:
-            return "3 flaws: in between gently used and roughly used"
-        if 4 >= self.condition > 3:
-            return "4 flaws: you're going to need a LOT of glue"
-        if 5 >= self.condition > 4:
-            return "5 flaws: gift it to someone you 'love'"
+        condition_descriptions = [
+            "0 flaws: the best you're going to get", 
+            "1 flaw: you're never going to financially recover from this",
+            "2 flaws: you might have buyer's remorse",
+            "3 flaws: in between gently used and roughly used",
+            "4 flaws: you're going to need a LOT of glue",
+            "5 flaws: gift it to someone you 'love'"
+            ]
+        return condition_descriptions[int(self.condition)]
