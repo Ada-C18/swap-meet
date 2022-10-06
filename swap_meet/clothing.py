37034552959ecb7345028738d8664cf1d5fd44d2
Diana Soriano <dssori@gmail.com>
2022-10-06 19:38:26 -0400

    merge

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,7 +2,7 @@ from swap_meet.item import Item
 
 class Clothing(Item):
     def __init__(self, age = 0, condition=0):
-        super().__init__(age, category="Clothing")
+        super().__init__(age, condition, category="Clothing")
 
     def __str__(self):
         return "The finest clothing you could wear."
