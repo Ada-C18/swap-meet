d4a16a51b389940f8a177bd254a0341a55b622f8
Milena Souto <yamadasouto@gmail.com>
2022-10-06 14:19:18 -0700

    Write asserts and pass test_swap_best_by_category_no_match_is_false

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -54,7 +54,7 @@ def test_best_by_category_with_duplicates():
     assert best_item.condition == pytest.approx(4.0)
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 def test_swap_best_by_category():
     # Arrange
     # me
@@ -99,7 +99,7 @@ def test_swap_best_by_category():
     assert item_f not in jesse.inventory
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 def test_swap_best_by_category_reordered():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -123,7 +123,7 @@ def test_swap_best_by_category_reordered():
         their_priority="Decor"
     )
 
-    # raise Exception("Complete this test according to comments below.")
+    raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
@@ -196,7 +196,7 @@ def test_swap_best_by_category_no_other_inventory_is_false():
     assert item_c in tai.inventory
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_swap_best_by_category_no_match_is_false():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -220,14 +220,25 @@ def test_swap_best_by_category_no_match_is_false():
         their_priority="Clothing"
     )
 
-    raise Exception("Complete this test according to comments below.")
+    # raise Exception("Complete this test according to comments below.")
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************
     # Assertions should check:
     # - That result is falsy
+    assert not result
     # - That tai and jesse's inventories are the correct length
+    assert len(tai.inventory) == 3
+    assert len(jesse.inventory) == 3
+
     # - That all the correct items are in tai and jesse's inventories
+    assert item_a in tai.inventory
+    assert item_b in tai.inventory
+    assert item_c in tai.inventory
+
+    assert item_d in jesse.inventory
+    assert item_e in jesse.inventory
+    assert item_f in jesse.inventory
 
 
 @pytest.mark.skip
