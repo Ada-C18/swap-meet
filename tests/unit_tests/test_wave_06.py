609f6ce041468db15066ff64fea3d27def0353fd
Sika Sarpong <50897076+sikasarp@users.noreply.github.com>
2022-10-06 07:25:12 -0700

    added unit test for age attribute-need more assertions i believe

--- tests/unit_tests/test_wave_06.py
+++ tests/unit_tests/test_wave_06.py
@@ -4,7 +4,9 @@ from swap_meet.clothing import Clothing
 from swap_meet.decor import Decor
 from swap_meet.electronics import Electronics
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_best_by_category():
     item_a = Clothing(condition=2.0)
     item_b = Decor(condition=2.0)
@@ -20,7 +22,9 @@ def test_best_by_category():
     assert best_item.category == "Clothing"
     assert best_item.condition == pytest.approx(4.0)
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_best_by_category_no_matches_is_none():
     item_a = Decor(condition=2.0)
     item_b = Decor(condition=2.0)
@@ -33,7 +37,9 @@ def test_best_by_category_no_matches_is_none():
 
     assert best_item is None
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_best_by_category_with_duplicates():
     # Arrange
     item_a = Clothing(condition=2.0)
@@ -50,7 +56,9 @@ def test_best_by_category_with_duplicates():
     assert best_item.category == "Clothing"
     assert best_item.condition == pytest.approx(4.0)
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_swap_best_by_category():
     # Arrange
     # me
@@ -89,7 +97,9 @@ def test_swap_best_by_category():
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories, including the items which were swapped from one vendor to the other
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_swap_best_by_category_reordered():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -128,7 +138,9 @@ def test_swap_best_by_category_reordered():
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories, and that the items that were swapped are not there
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_swap_best_by_category_no_inventory_is_false():
     tai = Vendor(
         inventory=[]
@@ -154,7 +166,9 @@ def test_swap_best_by_category_no_inventory_is_false():
     assert item_b in jesse.inventory
     assert item_c in jesse.inventory
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_swap_best_by_category_no_other_inventory_is_false():
     item_a = Clothing(condition=2.0)
     item_b = Decor(condition=4.0)
@@ -180,7 +194,9 @@ def test_swap_best_by_category_no_other_inventory_is_false():
     assert item_b in tai.inventory
     assert item_c in tai.inventory
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_swap_best_by_category_no_match_is_false():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -222,7 +238,9 @@ def test_swap_best_by_category_no_match_is_false():
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_swap_best_by_category_no_other_match_is_false():
     # Arrange
     item_a = Decor(condition=2.0)
@@ -263,3 +281,29 @@ def test_swap_best_by_category_no_other_match_is_false():
     # - That tai and jesse's inventories are the correct length
     # - That all the correct items are in tai and jesse's inventories
 
+
+# @pytest.mark.skip
+def test_swap_by_newest():
+    # Arrange:
+    item_a = Decor(age=3)
+    item_b = Electronics(age=4)
+    item_c = Decor(age=5)
+    tai = Vendor(
+        inventory=[item_a, item_b, item_c]
+    )
+
+    item_d = Clothing(age=2)
+    item_e = Decor(age=4)
+    item_f = Clothing(age=4)
+    jesse = Vendor(
+        inventory=[item_f, item_e, item_d]
+    )
+    # Act
+    result = tai.swap_by_newest(other=jesse, my_priority="Clothing",
+                                their_priority="Decor")
+    # Assert
+    assert result is None
+
+
+#  def test_swap_by_newest_with_no_match():
+    #  Arrange
