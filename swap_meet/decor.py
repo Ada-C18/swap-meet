3b8d6ae4d0aa566f9c303f6d278baae32b3b64e9
Nina <nina.sohn@gmail.com>
2022-10-06 16:59:10 -0700

    Added condition descriptions to Item class then had Clothing, Electronics, and Decor inherit

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,4 +1,6 @@
-class Decor:
+from swap_meet.item import Item
+
+class Decor(Item):
     
     def __init__(self, category = "Decor", condition = 0.0):
         self.category = category
@@ -6,17 +8,3 @@ class Decor:
 
     def __str__(self):
         return "Something to decorate your space."
-
-    def condition_description(self, condition = 0.0):
-        if self.condition == 0:
-            return "Revive me from the dead"
-        elif self.condition == 1:
-            return "I could use some love"
-        elif self.condition == 2:
-            return "Not too shabby, eh?"
-        elif self.condition == 3:
-            return "Great deal over here"
-        elif self.condition == 4:
-            return "I sparkle"
-        elif self.condition == 5:
-            return "I sparkle even in the darkle"
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,4 +1,6 @@
-class Electronics:
+from swap_meet.item import Item
+
+class Electronics(Item):
     
     def __init__(self, category = "Electronics", condition = 0.0):
         self.category = category
@@ -6,17 +8,3 @@ class Electronics:
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
-
-    def condition_description(self, condition = 0.0):
-        if self.condition == 0:
-            return "Revive me from the dead"
-        elif self.condition == 1:
-            return "I could use some love"
-        elif self.condition == 2:
-            return "Not too shabby, eh?"
-        elif self.condition == 3:
-            return "Great deal over here"
-        elif self.condition == 4:
-            return "I sparkle"
-        elif self.condition == 5:
-            return "I sparkle even in the darkle"
