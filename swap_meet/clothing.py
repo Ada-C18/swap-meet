bc55835f21703bc1aa954f1110c0e7a9e61f0a15
Eve Stroud <evestroud89@gmail.com>
2022-10-06 08:05:43 -0400

    Fix Wave 04 constructor

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,9 +1,9 @@
 from swap_meet.item import Item
 
-class Clothing(Item):
-    def __init__(self):
-        super().__init__(category="Clothing")
 
-    
+class Clothing(Item):
+    def __init__(self, condition=0):
+        super().__init__(condition, category="Clothing")
+
     def __str__(self):
         return "The finest clothing you could wear."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,8 +1,9 @@
 from swap_meet.item import Item
 
+
 class Electronics(Item):
-    def __init__(self):
-        super().__init__(category="Electronics")
+    def __init__(self, condition = 0):
+        super().__init__(condition, category="Electronics")
 
     def __str__(self):
         return "A gadget full of buttons and secrets."
