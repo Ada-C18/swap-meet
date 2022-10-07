563fe23f0a49fba9e534a557f34e91bd5a713116
Laura Skomerza <lskomerza@gmail.com>
2022-10-07 12:22:49 -0400

    updated swap_items and swap_first_item instances

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -26,4 +26,22 @@ class Vendor:
                     category_items.append(item)
             return category_items
 
-       
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory:
+            return False
+        if their_item not in other_vendor.inventory:
+            return False
+        self.remove(my_item) 
+        self.add(their_item)
+        other_vendor.remove(their_item)
+        other_vendor.add(my_item)
+        return True
+
+    def swap_first_item(self, another_vendor):
+        if len(self.inventory) == 0 or len(another_vendor.inventory) == 0:
+            return False
+        my_first_item = self.inventory[0]
+        their_first_item = another_vendor.inventory[0]
+        result = self.swap_items(another_vendor, my_first_item, their_first_item)
+        if result == True:
+            return True
