9eae1469ab3e0b88e0011b1810a1280c5c20a0e5
Abby <abbychoi97@gmail.com>
2022-10-07 13:37:56 -0700

    refactor condition description to account for floats

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,6 +1,6 @@
 class Item:
     def __init__(self, category="", condition=0.0, age=0):
-        '''takes in a category string, condition float, and age to instantiate an instance of Item'''
+        '''takes in a category string, condition float, and age integer to create an instance of Item'''
         self.category = category
         self.condition = condition
         self.age = age
@@ -10,22 +10,22 @@ class Item:
     
     def condition_description(self):
         '''returns a description of the item condition'''
-        condition = {
-            0:"Condition is unknown", 
-            1: "It needs lots of love",
-            2: "It needs some love",
-            3: "It just needs a little love",
-            4: "Very good",
-            5: "Like New!"
-            }
-        return condition[self.condition]
-        # if self.condition == 0: 
-        #     return "Condition is unknown"
-        # elif 1 <= self.condition < 3:
-        #     return "It needs a lot of love"
-        # elif self.condition == 3:
-        #     return "It just needs a little love"
-        # elif self.condition == 4: 
-        #     return  "Very good"
-        # elif self.condition == 5:
-        #     return "Like New!"
+        # condition = {
+        #     0:"Condition is unknown", 
+        #     1: "It needs lots of love",
+        #     2: "It needs some love",
+        #     3: "It just needs a little love",
+        #     4: "Very good",
+        #     5: "Like New!"
+        #     }
+        # return condition[self.condition]
+        if self.condition == 0: 
+            return "Condition is unknown"
+        elif 1 <= self.condition < 3:
+            return "It needs a lot of love"
+        elif 3 <= self.condition < 4:
+            return "It just needs a little love"
+        elif 4<= self.condition < 5: 
+            return  "Very good"
+        elif self.condition == 5:
+            return "Like New!"
