229036a9a816d33032bbe773fb3345faa3723333
Alaere Nekekpemi <alaeren00@gmail.com>
2022-10-07 09:53:47 -0700

    forgot to add in condition descriptions

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -9,4 +9,17 @@ class Item:
         return f"Hello World!"
 
     def condition_description(self):
-        return f"{self.condition}"
+        condition = int(self.condition)
+        if condition == 0:
+            return 'bad'
+        if condition == 1:
+            return 'poor'
+        if condition == 2:
+            return 'decent'
+        if condition == 3:
+            return 'average'
+        if condition == 4:
+            return 'good'
+        if condition == 5:
+            return 'pristine'
+        
