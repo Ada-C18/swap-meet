05b032db266b4563d1414534a24b276ad85c80ed
Tami <tamiadetayo@gmail.com>
2022-10-06 23:26:37 -0700

    Finished wave 4 method

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -25,11 +25,23 @@ class Vendor:
     def swap_items(self, vendor, my_item, their_item):
         if their_item not in vendor.inventory or my_item not in self.inventory:
             return False
-            
+
         self.remove(my_item)
         vendor.add(my_item)
         vendor.remove(their_item)
         self.add(their_item)
+        return True
+
+    def swap_first_item(self, vendor):
+        if not self.inventory or not vendor.inventory:
+            return False
+
+        instance_first_item = self.inventory[0]
+        vendor_first_item = vendor.inventory[0]
+        self.remove(instance_first_item)
+        self.add(vendor_first_item)
+        vendor.remove(vendor_first_item)
+        vendor.add(instance_first_item)
         return True
 
 
