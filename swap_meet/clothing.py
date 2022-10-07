4fa852f53d58cb7f6545254664b5e4c9c10342e1
Ariam Yonas <ariam.yonas@hotmail.de>
2022-10-07 09:44:38 -0700

    wave 5, still in progress

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,11 @@
+#from unicodedata import category
+from swap_meet.item import Item 
+
 class Clothing:
-    pass
+    def __init__(self, condition = 0):
+        super().__init__(category="Clothing", condition = condition)
+        
+        
+    def __str__(self):
+        return "The finest clothing you could wear."
+    
