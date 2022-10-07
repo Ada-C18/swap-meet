5d964b74d10088b8ba57106282fce9314c0578c2
Tapasya Khatri <tapasya@Tapasyas-MacBook-Air.local>
2022-10-07 00:18:10 -0700

    refactored test_adding_to_inventory method to allow only Item type arguments

--- tests/unit_tests/test_wave_01.py
+++ tests/unit_tests/test_wave_01.py
@@ -1,6 +1,10 @@
 # The following line imports the Vendor class from the module vendor inside the swap_meet package.
 import pytest
 from swap_meet.vendor import Vendor
+from swap_meet.item import Item
+from swap_meet.clothing import Clothing
+from swap_meet.decor import Decor
+from swap_meet.electronics import Electronics
 
 #@pytest.mark.skip
 def test_vendor_has_inventory():
@@ -19,8 +23,7 @@ def test_vendor_takes_optional_inventory():
 #@pytest.mark.skip
 def test_adding_to_inventory():
     vendor = Vendor()
-    item = "new item"
-
+    item =  Decor(age = 2, condition=4)
     result = vendor.add(item)
 
     assert len(vendor.inventory) == 1
