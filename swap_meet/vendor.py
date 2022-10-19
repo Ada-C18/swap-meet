c2d2b262ffeb9f1215697b9a7f4b282ac5b12139
Jacqueline Hopkins <318jlh@gmail.com>
2022-10-19 15:26:14 -0700

    def swap_first_item instance method for Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -28,6 +28,26 @@ class Vendor:
             if item.category == item_category.category:
                 items_in_category.append(item)
         return items_in_category
+    
+    def swap_items(self, vendor, my_item, their_item):
+        self.vendor = vendor
+        self.my_item = my_item
+        self.their_item = their_item
+        my_items = Vendor(self.inventory)
+        if my_item not in my_items.inventory or their_item not in vendor.inventory:
+            return False
+        my_items.remove(my_item)
+        my_items.add(their_item)
+        vendor.add(my_item)
+        vendor.remove(their_item)
+        return True
+    
+    def swap_first_item(self, vendor):
+        self.vendor = vendor
+        if not self.inventory or not vendor.inventory:
+            return False
+        return self.swap_items(vendor, self.inventory[0], vendor.inventory[0])
+
 
 
     #     items_in_category = []
