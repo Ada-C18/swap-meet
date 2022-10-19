0608a44f22d87d2c1418ed10a142c74299813979
Jacqueline Hopkins <318jlh@gmail.com>
2022-10-19 15:23:43 -0700

    updated category to be assined to an empty string instead of an empty dictionary

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,12 +1,23 @@
 class Item:
 
-    category = {}
+    category = ""
     
-    def __init__(self, category=""):
+    def __init__(self, category=category):
         self.category = category
     
     def __str__(self):
         return ("Hello World!")
+    
+    def condition_description(self, condition_rate):
+        condition_dict = {
+            "0": "Used with visible flaws",
+            "1": "Used with minor flaws",
+            "2": "Used with little to no flaws",
+            "3": "Used with no flaws",
+            "4": "Like new",
+            "5": "Perfect condition"
+        }
+        return(condition_dict[condition_rate])
 
     
 
