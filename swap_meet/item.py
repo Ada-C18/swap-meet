826c303de45f432ea46eb5182b52095c7a6b052d
Jacqueline Hopkins <318jlh@gmail.com>
2022-10-19 12:09:43 -0700

    previous commit was actually to run wave 03 test. Def __str__ instance method for Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -5,8 +5,8 @@ class Item:
     def __init__(self, category=""):
         self.category = category
     
-    # def __iter__(self):
-    #     return self
+    def __str__(self):
+        return ("Hello World!")
 
     
 
