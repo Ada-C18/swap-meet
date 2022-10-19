9c7423635c9a34010c06c14c0fd75fe07490576d
Jacqueline Hopkins <318jlh@gmail.com>
2022-10-19 02:32:10 -0700

    define get_by_category instance method in Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,3 +1,4 @@
+from swap_meet.item import Item
 class Vendor:
     
     def __init__(self, inventory=[]):
@@ -13,4 +14,31 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+
+    # create get_by_cateogry instance method that takes in category argument
+
+    def get_by_category(self, category):
+    #     # reutrn list of items by category
+        self.category = category
+        items_in_category = []
+        # instance from item class
+        item_category = Item(category=self.category)
+        for item in self.inventory:
+            # item_category = Item(category=self.category)
+            if item.category == item_category.category:
+                items_in_category.append(item)
+        return items_in_category
+
+
+    #     items_in_category = []
+    #     for i in self.inventory:
+    #         if i == Item(category=category):
+    #             items_in_category.append(i)
+        
+    #     return items_in_category
+
+        
+        # Item.category
+
+
 
