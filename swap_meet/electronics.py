a62ccca066b05f34dc5b8207d1ef7b0536535b2e
Jacqueline Hopkins <318jlh@gmail.com>
2022-10-19 15:29:00 -0700

    create electronic child class

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,14 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    
+    category = "Electronics"
+    def __init__(self, category=category):
+        super().__init__(self)
+        self.category = category
+
+    # def __init__(self, category):
+    #     self.category = category
+    
+    def __str__(self):
+        return ("A gadget full of buttons and secrets.")
