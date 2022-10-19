6ed61d0b16c68bc705a05e88a9f7cd20dcee5420
Jacqueline Hopkins <318jlh@gmail.com>
2022-10-19 15:25:15 -0700

    created clothing class as a child class to Item

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,14 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    
+    category = "Clothing"
+    def __init__(self, category=category):
+        super().__init__(self)
+        self.category = category
+
+    # def __init__(self, category):
+    #     self.category = category
+    
+    def __str__(self):
+        return ("The finest clothing you could wear.")
