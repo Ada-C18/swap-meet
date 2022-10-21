0fead23a7cd0d609d125f86983615a786f273c40
Jacqueline Hopkins <318jlh@gmail.com>
2022-10-20 19:28:01 -0700

    def condition descripttion instance method in Item class

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,25 +1,37 @@
 class Item:
-
-    category = ""
     
-    
-    def __init__(self, category=category, condition = 0):
+    def __init__(self, category= "", condition = 0):
         self.category = category
         self.condition = condition
     
     def __str__(self):
         return ("Hello World!")
+
+
+    # conditional statements 
     
-    def condition_description(self, condition_rate):
-        condition_dict = {
-            "0": "Used with visible flaws",
-            "1": "Used with minor flaws",
-            "2": "Used with little to no flaws",
-            "3": "Used with no flaws",
-            "4": "Like new",
-            "5": "Perfect condition"
-        }
-        return(condition_dict[condition_rate])
+    def condition_description(self):
+
+        if self.condition == 5:
+            description = "Perfect"
+        elif 0 <= self.condition <= 2:
+            description = "Used with minor flaws"
+        elif 2.1 <= self.condition <= 3:
+            description = "Used with little to no flaws"
+        elif 3.1 <= self.condition <= 4.9:
+            description = "Like New"
+        return description
+        
+    
+    #    condition_dict = {
+    #         "0": "Used with visible flaws",
+    #         "1": "Used with minor flaws",
+    #         "2": "Used with little to no flaws",
+    #         "3": "Used with no flaws",
+    #         "4": "Like new",
+    #         "5": "Perfect condition"
+    #     }
+    #     return(condition_dict[condition_rate])
 
     
 
