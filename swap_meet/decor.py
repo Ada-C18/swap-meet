6195500d321b21bf37436fd84b3f4e0be4df33c2
Nancy Lee <nramirez289@gmail.com>
2022-10-23 19:24:10 -0700

    Used super method in clothing, decor, electronics

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -2,8 +2,11 @@ from swap_meet.item import Item
 
 class Decor(Item):
     def __init__(self, category="Decor", condition=0):
-        self.category = category
-        self.condition = condition
+        super().__init__(category, condition)
+
+    # def __init__(self, category="Decor", condition=0):
+    #     self.category = category
+    #     self.condition = condition
 
     def __str__ (self):
         return "Something to decorate your space."
--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -2,8 +2,11 @@ from swap_meet.item import Item
 
 class Electronics(Item):
     def __init__(self, category="Electronics", condition=0):
-        self.category = category
-        self.condition= condition
+        super().__init__(category, condition)
+
+    # def __init__(self, category="Electronics", condition=0):
+    #     self.category = category
+    #     self.condition= condition
 
     def __str__ (self):
         return "A gadget full of buttons and secrets."
